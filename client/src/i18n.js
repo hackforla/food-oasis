44e030f925397cb0cbda4a607dd4201b430d30e1
Nicholas Pung <nckpng95@gmail.com>
2019-10-15 20:39:05 -0700

    Added packages for auto language detection, created folders to store translation text

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,27 +1,31 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
-// the translations
-// (tip move them in a JSON file and import them)
-const resources = {
-  en: {
-    translation: {
-      "Welcome to React": "Welcome to React and react-i18next"
-    }
-  }
-};
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+// not like to use this?
+// have a look at the Quick start guide 
+// for passing in lng and translations on init
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  // load translation using xhr -> see /public/locales
+  // learn more: https://github.com/i18next/i18next-xhr-backend
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    resources,
-    lng: "en",
-
-    keySeparator: false, // we do not use keys in form messages.welcome
+    fallbackLng: 'en',
+    debug: true,
 
     interpolation: {
-      escapeValue: false // react already safes from xss
+      escapeValue: false, // not needed for react as it escapes by default
     }
   });
 
-  export default i18n;
+
+export default i18n;

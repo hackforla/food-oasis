4663affd31c87f9a5d640e25bbd19746f960a9e1
Nicholas Pung <nckpng95@gmail.com>
2019-11-02 21:22:30 -0700

    Added content for About/Donate with translation layout, with no styling added.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,43 +2,43 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 const About = () => {
-  const { t, i18n } = useTranslation("translation");
+  const { t } = useTranslation("about");
   return (
     <>
-      <h1>{t("about.title")}</h1>
-      <p>{t("about.p1")}</p>
-      <p>{t("about.p2")}</p>
-      <h2>{t("about.support")}</h2>
-      <p>{t("about.support-p")}</p>
-      <button>{t("about.donate")}</button>
-      <h2>{t("about.use")}</h2>
+      <h1>{t("title")}</h1>
+      <p>{t("p1")}</p>
+      <p>{t("p2")}</p>
+      <h2>{t("support")}</h2>
+      <p>{t("support-p")}</p>
+      <button>{t("donate")}</button>
+      <h2>{t("use")}</h2>
       <div>
-        <h2>{t("about.stakeholder-1")}</h2>
+        <h2>{t("stakeholder-1")}</h2>
         <p>
-          <strong>{t("about.stakeholder-1ps")}</strong>
+          <strong>{t("stakeholder-1ps")}</strong>
         </p>
-        <p>{t("about.stakeholder-1p")}</p>
+        <p>{t("stakeholder-1p")}</p>
       </div>
       <div>
-        <h2>{t("about.stakeholder-2")}</h2>
+        <h2>{t("stakeholder-2")}</h2>
         <p>
-          <strong>{t("about.stakeholder-2ps")}</strong>
+          <strong>{t("stakeholder-2ps")}</strong>
         </p>
-        <p>{t("about.stakeholder-2p")}</p>
+        <p>{t("stakeholder-2p")}</p>
       </div>
       <div>
-        <h2>{t("about.stakeholder-3")}</h2>
+        <h2>{t("stakeholder-3")}</h2>
         <p>
-          <strong>{t("about.stakeholder-3ps")}</strong>
+          <strong>{t("stakeholder-3ps")}</strong>
         </p>
-        <p>{t("about.stakeholder-3p")}</p>
+        <p>{t("stakeholder-3p")}</p>
       </div>
-      <h2>{t("about.mission")}</h2>
-      <p>{t("about.mission-p")}</p>
-      <h2>{t("about.team")}</h2>
-      <p>{t("about.team-p")}</p>
-      <h2>{t("about.about-hack")}</h2>
-      <p>{t("about.about-hack-p")}</p>
+      <h2>{t("mission")}</h2>
+      <p>{t("mission-p")}</p>
+      <h2>{t("team")}</h2>
+      <p>{t("team-p")}</p>
+      <h2>{t("about-hack")}</h2>
+      <p>{t("about-hack-p")}</p>
     </>
   );
 };

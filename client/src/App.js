e14f3e0d4d81aa21556b920329b376338807337f
Nicholas Pung <nckpng95@gmail.com>
2019-11-09 13:50:57 -0800

    Added FAQ component, need to check if it's pulling from database correctly

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Footer from "./components/Footer";
 import Organizations from "./components/Organizations";
+import Faq from './components/Faq'
 
 const styles = {
   app: {
@@ -80,6 +81,9 @@ function App() {
           </Route>
           <Route path="/donate">
             <Donate />
+          </Route>
+          <Route path="/faqs">
+            <Faq />
           </Route>
           <Route path="/news">
             <News />
---
+++ client/src/components/Faq.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useTranslation } from "react-i18next";
+
+const Faq = () => {
+  // Load in current FAQs
+  // If FAQs need to be updated, go into admin portal
+  // This will only display the current FAQs
+
+  const [faqs, setFaqs] = useState([]);
+  const { t } = useTranslation("faq");
+
+  // Should I create a faq-service? to follow current design pattern, or can this stay here?
+  useEffect(() => {
+    async function fetchFaqs() {
+      const response = await axios.get("/api/faqs");
+      console.log(response);
+      // if response is an array
+      setFaqs(response);
+    }
+    fetchFaqs();
+  }, []);
+  
+  return (
+    <>
+      <p>{t("title")}</p>
+      {faqs.map(faq => (
+        <div>
+          <p>Question</p>
+          <p>Answer</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Faq;

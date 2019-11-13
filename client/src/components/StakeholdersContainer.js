091b8d1bea2e4f50a4c79e20faa0ac0163668c84
Lucas Homer <lucashomer@lucass-mbp.home>
2019-11-12 17:11:59 -0800

    hook up Map to StakeholdersContainer and introduce button to toggle list vs map views

--- client/src/components/StakeholdersContainer.js
+++ client/src/components/StakeholdersContainer.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import MapIcon from "@material-ui/icons/Map";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+import { Typography, IconButton } from "@material-ui/core";
 import StakeholderSearch from "./StakeholderSearch";
 import StakeholderCriteria from "./StakeholderCriteria";
 import StakeholderList from "./StakeholderList";
+import Map from "./Map";
 import { useStakeholders } from "../hooks/useStakeholders/useStakeholders";
 
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    padding: "1rem",
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+};
 function StakeholdersContainer() {
   const { state, dispatch, actionTypes, search } = useStakeholders();
+  const [isMapView, setIsMapView] = React.useState(true);
 
   const openSearchPanel = isOpen => {
     dispatch({ type: actionTypes.TOGGLE_SEARCH_PANEL, isOpen });
@@ -25,39 +40,61 @@ function StakeholdersContainer() {
   } = state;
 
   return (
-    <React.Fragment>
-      <Typography
-        variant={"h4"}
-        component={"h1"}
-        align="center"
-        style={{ marginBottom: "0.5em" }}
-      >
-        Stakeholders{" "}
-      </Typography>
-      {isSearchPanelOpen ? (
-        <StakeholderSearch
-          key={selectedLatitude}
-          latitude={selectedLatitude}
-          longitude={selectedLongitude}
-          categories={categories}
-          searchString={searchString}
-          selectedCategories={selectedCategories}
-          selectedDistance={selectedDistance}
-          search={search}
-        />
-      ) : (
-        <StakeholderCriteria
-          key={selectedLatitude}
-          latitude={selectedLatitude}
-          longitude={selectedLongitude}
-          searchString={searchString}
-          selectedCategories={selectedCategories}
-          selectedDistance={selectedDistance}
-          openSearchPanel={openSearchPanel}
-        />
-      )}
-      {isLoading ? null : <StakeholderList stakeholders={stakeholders} />}
-    </React.Fragment>
+    <div style={styles.container}>
+      <div style={styles.header}>
+        <Typography
+          variant={"h4"}
+          component={"h1"}
+          align="center"
+          style={{ marginBottom: "0.5em" }}
+        >
+          Stakeholders{" "}
+        </Typography>
+        <IconButton
+          onClick={() => setIsMapView(!isMapView)}
+          title={`Go to ${isMapView ? "List" : "Map"}`}
+        >
+          {isMapView && <FormatListBulletedIcon />}
+          {!isMapView && <MapIcon style={{ color: "#90C146" }} />}
+        </IconButton>
+      </div>
+      <div>
+        {isSearchPanelOpen ? (
+          <StakeholderSearch
+            key={selectedLatitude}
+            latitude={selectedLatitude}
+            longitude={selectedLongitude}
+            categories={categories}
+            searchString={searchString}
+            selectedCategories={selectedCategories}
+            selectedDistance={selectedDistance}
+            search={search}
+          />
+        ) : (
+          <StakeholderCriteria
+            key={selectedLatitude}
+            latitude={selectedLatitude}
+            longitude={selectedLongitude}
+            searchString={searchString}
+            selectedCategories={selectedCategories}
+            selectedDistance={selectedDistance}
+            openSearchPanel={openSearchPanel}
+          />
+        )}
+        {/* TODO: make a loading component! */}
+        {isLoading ? (
+          <h3>Loading...</h3>
+        ) : isMapView ? (
+          <Map
+            stakeholders={stakeholders}
+            selectedLatitude={selectedLatitude}
+            selectedLongitude={selectedLongitude}
+          />
+        ) : (
+          <StakeholderList stakeholders={stakeholders} />
+        )}
+      </div>
+    </div>
   );
 }
 

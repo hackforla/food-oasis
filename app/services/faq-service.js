4fb005704e5eb6807b55d09320a1bb42b036ff39
Nicholas Pung <nckpng95@gmail.com>
2019-11-13 18:00:15 -0800

    Started working on figuring out how to post with translations

--- app/services/faq-service.js
+++ app/services/faq-service.js
@@ -1,39 +1,40 @@
 const { pool } = require("./postgres-pool");
 
-const selectAll = () => {
+const selectAll = language => {
   const sql = `
-    select id, question, answer
+    select id, question, answer, language
     from faq
     order by id
+    where language = $1
   `;
-  return pool.query(sql).then(res => {
+  return pool.query(sql, [language]).then(res => {
     return res.rows;
   });
 };
 
-const selectById = id => {
-  const sql = `select id, question, answer from faq where id = $1`;
-  return pool.query(sql, [id]).then(res => {
+const selectById = (id, language) => {
+  const sql = `select id, question, answer, language from faq where id = $1 and language = $2`;
+  return pool.query(sql, [id, language]).then(res => {
     return res.rows[0];
   });
 };
 
 const insert = model => {
-  const { question, answer } = model;
-  const sql = `insert into faq (question, answer) values ($1, $2) returning id`;
-  return pool.query(sql, [question, answer]).then(res => {
+  const { question, answer, language } = model;
+  const sql = `insert into faq (question, answer, language) values ($1, $2, $3) returning id`;
+  return pool.query(sql, [question, answer, language]).then(res => {
     return res.rows[0];
   });
 };
 
 const update = (id, model) => {
-  const { question, answer } = model;
+  const { question, answer, language } = model;
   const sql = `
     update faq
-    set question = $1, answer = $2
-    where id = $3 returning *
+    set question = $1, answer = $2, language = $3
+    where id = $4 returning *
   `;
-  return pool.query(sql, [question, answer, id]).then(res => {
+  return pool.query(sql, [question, answer, language, id]).then(res => {
     return res.rows[0];
   });
 };
@@ -52,3 +53,25 @@ module.exports = {
   update,
   remove
 };
+
+/*
+Concern: when I add a new FAQ, I need both (ex.) English and Spanish to be posted.
+ex. [
+  {question: "", answer: "", language: "en"}, {question: "", answer: "", language: "es"}
+]
+How I see it, is that I do a map on the array coming in, and add a new FAQ with a new id for each.
+When I update, I want to pull both English and Spanish versions from one of the ids.
+Then do a map and update both objects from their specific ids.
+When I delete, I want to delete both English and Spanish versions from one of the ids.
+
+1 idea
+Data Tables
+
+faq_identifier
+unique key -- general question
+
+faq
+unique key -- primary key(faq_identifier) -- question -- answer -- language
+
+- Don't think I will be using selectById
+*/

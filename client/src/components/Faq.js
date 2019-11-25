e4c0947aa87524ad78bee1d7a2021ca9a8581712
Nicholas Pung <nckpng95@gmail.com>
2019-11-25 10:11:23 -0800

    Tried connecting FAQ page with FAQ backend, still working on it

--- client/src/components/Faq.js
+++ client/src/components/Faq.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useTranslation } from "react-i18next";
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import * as faqService from '../services/faq-service';
 
 const Faq = () => {
   // Load in current FAQs
@@ -8,30 +8,34 @@ const Faq = () => {
   // This will only display the current FAQs
 
   const [faqs, setFaqs] = useState([]);
-  const { t } = useTranslation("faq");
+  const { t, i18n } = useTranslation('faq');
 
   // Should I create a faq-service? to follow current design pattern, or can this stay here?
   // Query in the english version of FAQs
   useEffect(() => {
     async function fetchFaqs() {
-      const response = await axios.get("/api/faqs");
-      console.log(response);
-      // if response is an array
-      setFaqs(response);
+      const fetchedFaqs = await faqService.getAll({ language: i18n.language });
+      setFaqs(fetchedFaqs);
     }
     fetchFaqs();
-  }, []);
+  }, [i18n.language]);
+
+  console.log(faqs);
 
   return (
     <>
-      <p>{t("title")}</p>
-      {faqs.map(faq => (
-        <div>
-          {/* ex. Loading: t(faq.question), t(faq.answer) */}
-          <p>Question</p>
-          <p>Answer</p>
-        </div>
-      ))}
+      <p>{t('title')}</p>
+      {faqs[0] ? (
+        faqs.map(faq => (
+          <div>
+            {/* ex. Loading: t(faq.question), t(faq.answer) */}
+            <p>Question</p>
+            <p>Answer</p>
+          </div>
+        ))
+      ) : (
+        <div>FAQs are loading...</div>
+      )}
     </>
   );
 };

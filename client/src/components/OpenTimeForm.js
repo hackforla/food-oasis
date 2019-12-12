bcfacc90f9fbc097cd9e19c90efb98ba8d2a2ee7
John Darragh <darragh@entrotech.net>
2019-12-11 21:57:03 -0800

    comment out more code in OpenTimeForm

--- client/src/components/OpenTimeForm.js
+++ client/src/components/OpenTimeForm.js
@@ -13,14 +13,14 @@ function OpenTimeForm(props) {
     }
   ]);
 
-  useEffect(() => {
-    // uses the handleChange from Formik to change form
-    handleChange({ target: { value: inputs, name: "hours" } });
-  }, [inputs, handleChange]);
+  // useEffect(() => {
+  //   // uses the handleChange from Formik to change form
+  //   handleChange({ target: { value: inputs, name: "hours" } });
+  // }, [inputs, handleChange]);
 
-  useEffect(() => {
-    setInputs(originalData);
-  }, [originalData]);
+  // useEffect(() => {
+  //   setInputs(originalData);
+  // }, [originalData]);
 
   const addInput = () => {
     let newList = [
@@ -30,58 +30,58 @@ function OpenTimeForm(props) {
     setInputs(newList);
   };
 
-  const removeInput = index => {
-    let newList = inputs.slice().filter((val, i) => i !== index);
-    setInputs(newList);
-  };
+  // const removeInput = index => {
+  //   let newList = inputs.slice().filter((val, i) => i !== index);
+  //   setInputs(newList);
+  // };
 
-  const handleTime = number => {
-    //formats time input into HH:MM:SS format
-    let output = "";
-    number.replace(
-      /^\D*(\d{0,2})\D*(\d{0,2})\D*(\d{0,2})/,
-      (match, hh, mm, ss) => {
-        if (hh.length) {
-          output += hh;
-          if (mm.length) {
-            output += `:${mm}`;
-            if (ss.length) {
-              output += `:${ss}`;
-            }
-          }
-        }
-      }
-    );
-    return output;
-  };
+  // const handleTime = number => {
+  //   //formats time input into HH:MM:SS format
+  //   let output = "";
+  //   number.replace(
+  //     /^\D*(\d{0,2})\D*(\d{0,2})\D*(\d{0,2})/,
+  //     (match, hh, mm, ss) => {
+  //       if (hh.length) {
+  //         output += hh;
+  //         if (mm.length) {
+  //           output += `:${mm}`;
+  //           if (ss.length) {
+  //             output += `:${ss}`;
+  //           }
+  //         }
+  //       }
+  //     }
+  //   );
+  //   return output;
+  // };
 
-  const inputsMap = inputs.map((val, i) => {
-    // let stateChange = (value, name) => {
-    //   let newList = [...inputs];
-    //   if (name === "open" || name === "close") {
-    //     newList[i][name] = handleTime(value);
-    //   } else {
-    //     newList[i][name] = value;
-    //   }
-    //   setInputs(newList);
-    // };
+  // const inputsMap = inputs.map((val, i) => {
+  //   // let stateChange = (value, name) => {
+  //   //   let newList = [...inputs];
+  //   //   if (name === "open" || name === "close") {
+  //   //     newList[i][name] = handleTime(value);
+  //   //   } else {
+  //   //     newList[i][name] = value;
+  //   //   }
+  //   //   setInputs(newList);
+  //   // };
 
-    return (
-      <div key={i}>
-        {i}
-        {/* <OpenTimeInputs
-            values={val}
-            stateChange={stateChange}
-            removeInput={removeInput}
-            index={i}
-          /> */}
-      </div>
-    );
-  });
+  //   return (
+  //     <div key={i}>
+  //       {i}
+  //       {/* <OpenTimeInputs
+  //           values={val}
+  //           stateChange={stateChange}
+  //           removeInput={removeInput}
+  //           index={i}
+  //         /> */}
+  //     </div>
+  //   );
+  // });
 
   return (
     <div>
-      <div>{inputsMap}</div>
+      {/* <div>{inputsMap}</div> */}
       <Button onClick={addInput}>Add Another Time</Button>
     </div>
   );

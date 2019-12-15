e2a8dd765b52ac94d4cbb70b9a2110aa2b46f05d
Lucas Homer <lucashomer@Lucass-MacBook-Pro.local>
2019-12-15 09:46:18 -0800

    setup basic container component for map and searchbar work

---
+++ client/src/components/SearchPage.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactMapGL from "react-map-gl";
+import { makeStyles } from "@material-ui/core/styles";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+
+import FilterMenu from "./FilterMenu";
+import CurrentLocationIcon from "./CurrentLocationIcon";
+import { MAPBOX_TOKEN } from "../secrets";
+import { MAPBOX_STYLE } from "../constants/map";
+
+const useStyles = makeStyles(theme => ({
+  searchBarContainer: {
+    backgroundColor: "white",
+    display: "flex",
+    justifyContent: "flex-start",
+    margin: ".5rem",
+    boxShadow: "0px 2px 10px -1px #bfbfbf",
+    borderRadius: "30px",
+  },
+}));
+
+function SearchPage() {
+  const classes = useStyles();
+  const [viewport, setViewport] = React.useState({
+    zoom: 10, // TODO: can we dynamically control zoom radius based on selectedDistance?
+    latitude: 34.041001,
+    longitude: -118.235036,
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      width={`100vw`}
+      height={`90vh`}
+      onViewportChange={newViewport => setViewport(newViewport)}
+      mapboxApiAccessToken={MAPBOX_TOKEN}
+      mapStyle={MAPBOX_STYLE}
+    >
+      {/*
+
+
+      StakeholderList
+      SearchResults --> Markers
+      */}
+      <div className={classes.searchBarContainer}>
+        <FilterMenu />
+        <CurrentLocationIcon isTrackingEnabled={false} />
+        <div>🔎 LocationAutoComplete</div>
+      </div>
+    </ReactMapGL>
+  );
+}
+
+export default SearchPage;

28377d7af6b2bd24b30e9742aaed7b9b807bd12a
Lucas Homer <lucashomer@lucass-mbp.home>
2020-02-06 20:02:18 -0800

    updating styles and ui cleanup for FAQ page

--- client/src/components/FaqList.js
+++ client/src/components/FaqList.js
@@ -4,17 +4,26 @@ import { makeStyles } from "@material-ui/styles";
 import FaqItem from "./FaqItem";
 
 const useStyles = makeStyles({
+  readOnly: {
+    listStyle: "none",
+    padding: "0",
+    display: "flex",
+    flexDirection: "column",
+  },
   edit: {
     listStyle: "none",
-    fontSize: "2rem"
-  }
+    fontSize: "2rem",
+    padding: "0",
+    display: "flex",
+    flexDirection: "column",
+  },
 });
 
 const FaqList = ({ faqs, message, reorder, reorderFaqs }) => {
   const classes = useStyles();
 
   return faqs.length ? (
-    <ul className={reorder ? classes.edit : ""}>
+    <ul className={reorder ? classes.edit : classes.readOnly}>
       {faqs.map(faq => (
         <FaqItem
           faq={faq}

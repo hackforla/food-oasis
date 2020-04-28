f0b3feeeb4c7450d19997a37f700a52f9cb33d43
HassenH1 <hasansaid51@gmail.com>
2020-04-27 17:25:18 -0700

    header

--- client/src/components/Verification/SecurityAdminDashboard.js
+++ client/src/components/Verification/SecurityAdminDashboard.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { makeStyles } from "@material-ui/styles";
+import Typography from '@material-ui/core/Typography';
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+
+
+const useStyles = makeStyles({
+  root: {
+    textAlign: "center",
+    padding: "2rem",
+  },
+  image: {
+    margin: "1rem",
+  },
+  sectionHeader: {
+    marginTop: "3rem",
+  },
+  description: {
+    margin: "1rem 0",
+  },
+});
 
 function SecurityAdminDashboard() {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      <h3>SecurityAdminDashboard!!!</h3>
-    </div>
+    <Container maxWidth="sm" className={classes.root}>
+      <Typography variant="h4">Security Roles</Typography>
+    </Container>
   )
 }
 

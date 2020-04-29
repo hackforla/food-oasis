2e777e138ffe0891b18a48e4f57cc03fcc5a95d4
Tim Malstead <timmmalstead@gmail.com>
2020-04-29 13:58:03 -0700

    styles results list further

--- client/src/components/ResultsList.js
+++ client/src/components/ResultsList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
+import pantryIcon from "../images/pantryIcon.svg"
+import mealIcon from "../images/mealIcon.svg"
 
 const useStyles = makeStyles((theme) => ({
   stakeholderArrayHolder: {
@@ -16,17 +18,25 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-between",
     padding: "1em 0",
     borderBottom: " .2em solid #f1f1f1"
+  }, imgHolder: {
+    // backgroundColor: "red",
+    display: "inherit",
+    justifyContent: "center",
+    alignItems: "center",
   },
   typeLogo: {
-    width: "20%",
-    height: "auto",
-    backgroundColor: "red"
+    width: "72px",
   },
   infoHolder: {
-    width: "75%",
+    fontSize: "1.1em",
+    textAlign: "left",
+    width: "60%",
     display: "inherit",
     flexDirection: "column",
-    alignItems: "flex-start"
+    justifyContent: "space-between"
+  },
+  checkHolder: {
+    width: "10%"
   }
 }))
 
@@ -38,10 +48,21 @@ const ResultsList = ({ selectedStakeholder, stakeholders }) => {
   return <div className={classes.stakeholderArrayHolder}>
     {stakeholders ? stakeholders.map((stakeholder) =>
       <div className={classes.stakeholderHolder}>
-        <div className={classes.typeLogo} />
+        <div className={classes.imgHolder}>
+          <img src={stakeholder.categories[0].id === 1 ? pantryIcon : mealIcon}
+            className={classes.typeLogo} />
+        </div>
         <div className={classes.infoHolder}>
           <span>{stakeholder.name}</span>
           <span>{stakeholder.address1}</span>
+          <em style={{ color: stakeholder.categories[0].id === 1 ? "#336699" : "#CC3333" }}>
+            {stakeholder.categories[0].name}
+          </em>
+        </div>
+        <div className={classes.checkHolder}>
+          {stakeholder.distance >= 10 ?
+            stakeholder.distance.toString().substring(0, 3).padEnd(4, "0") :
+            stakeholder.distance.toString().substring(0, 3)} mi
         </div>
       </div>
     ) : null}

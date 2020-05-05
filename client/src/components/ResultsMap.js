75973322292fb9230faa314a747626f0c2f0b2ff
John Darragh <darragh@entrotech.net>
2020-05-04 17:47:50 -0700

    Show submitted organizations as checked on map markers

--- client/src/components/ResultsMap.js
+++ client/src/components/ResultsMap.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactMapGL, { NavigationControl } from "react-map-gl";
-import MarkerPopup from "./MarkerPopup";
-import Marker from "./Marker";
-import { MAPBOX_TOKEN } from "../secrets";
+import React from 'react'
+import ReactMapGL, { NavigationControl } from 'react-map-gl'
+import MarkerPopup from './MarkerPopup'
+import Marker from './Marker'
+import { MAPBOX_TOKEN } from '../secrets'
 import {
   DEFAULT_CATEGORIES,
   FOOD_PANTRY_CATEGORY_ID,
@@ -11,18 +11,18 @@ import {
   MEAL_PROGRAM_CATEGORY_ID,
   ORGANIZATION_COLORS,
   PANTRY,
-} from "../constants/map";
-import moment from "moment";
+} from '../constants/map'
+import moment from 'moment'
 
 const styles = {
   geolocate: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
     margin: 10,
   },
-  navigationControl: { position: "absolute", top: 0, right: 0, margin: 10 },
-};
+  navigationControl: { position: 'absolute', top: 0, right: 0, margin: 10 },
+}
 
 function Map({
   selectedLatitude = 34.07872,
@@ -31,37 +31,37 @@ function Map({
   categoryIds,
 }) {
   React.useEffect(() => {
-    console.log("map", stakeholders);
-  }, [stakeholders]);
+    console.log('map', stakeholders)
+  }, [stakeholders])
 
   const categoryIdsOrDefault = categoryIds.length
     ? categoryIds
-    : DEFAULT_CATEGORIES;
+    : DEFAULT_CATEGORIES
 
-  const [isPopupOpen, setIsPopupOpen] = React.useState(false);
-  const [selectedStakeholder, setSelectedStakeholder] = React.useState(null);
+  const [isPopupOpen, setIsPopupOpen] = React.useState(false)
+  const [selectedStakeholder, setSelectedStakeholder] = React.useState(null)
   const [viewport, setViewport] = React.useState({
     zoom: 10, // TODO: can we dynamically control zoom radius based on selectedDistance?
     latitude: selectedLatitude,
     longitude: selectedLongitude,
-  });
+  })
 
   const handleMarkerClick = (clickedStakeholder) => {
-    setSelectedStakeholder(clickedStakeholder);
-    setIsPopupOpen(true);
-  };
+    setSelectedStakeholder(clickedStakeholder)
+    setIsPopupOpen(true)
+  }
 
   const handleClose = () => {
-    setIsPopupOpen(false);
-    setSelectedStakeholder(null);
-  };
+    setIsPopupOpen(false)
+    setSelectedStakeholder(null)
+  }
 
   return (
     <div>
       <ReactMapGL
         {...viewport}
-        width={`100%`}
-        height={`max(calc(100vh - 250px),47em)`}
+        width="100%"
+        height="max(calc(100vh - 250px),47em)"
         onViewportChange={(newViewport) => setViewport(newViewport)}
         mapboxApiAccessToken={MAPBOX_TOKEN}
         mapStyle={MAPBOX_STYLE}
@@ -73,22 +73,27 @@ function Map({
           stakeholders
             .filter((sh) => sh.latitude && sh.longitude)
             .map((stakeholder, index) => {
-              const isVerified = !!stakeholder.verifiedDate; //can we assume it will be a date of some sort?
+              // The verifiedDate property was removed from the
+              // stakeholder object. For now, show the stakeholders with
+              // a submittedDate as "verified", since there are engough
+              // of them to work with. This might change to approvedDate
+              // or something later.
+              const isVerified = !!stakeholder.submittedDate
               /*todo
                * implement condition based on api data
                * */
 
               const categories = stakeholder.categories.filter(({ id }) => {
-                return categoryIdsOrDefault.includes(id);
-              });
+                return categoryIdsOrDefault.includes(id)
+              })
 
-              console.log(categories);
+              console.log(categories)
 
               const color = categories.find(
-                ({ id }) => id === MEAL_PROGRAM_CATEGORY_ID
+                ({ id }) => id === MEAL_PROGRAM_CATEGORY_ID,
               )
                 ? ORGANIZATION_COLORS[MEAL_PROGRAM_CATEGORY_ID]
-                : ORGANIZATION_COLORS[FOOD_PANTRY_CATEGORY_ID];
+                : ORGANIZATION_COLORS[FOOD_PANTRY_CATEGORY_ID]
               return (
                 <Marker
                   onClick={() => handleMarkerClick(stakeholder)}
@@ -98,14 +103,14 @@ function Map({
                   isVerified={isVerified}
                   color={color}
                 />
-              );
+              )
             })}
         {isPopupOpen && selectedStakeholder && (
           <MarkerPopup entity={selectedStakeholder} handleClose={handleClose} />
         )}
       </ReactMapGL>
     </div>
-  );
+  )
 }
 
-export default Map;
+export default Map

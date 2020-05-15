ba54986a5f195f6bbcb93f7b9d24b4b2b9815b1e
Tim Malstead <timmmalstead@gmail.com>
2020-05-15 15:05:22 -0700

    fixes split pantry/meals icon and identifies issue with anne douglas center los angeles mission hours

--- client/src/components/ResultsMap.js
+++ client/src/components/ResultsMap.js
@@ -28,9 +28,12 @@ function Map({
   stakeholders,
   categoryIds,
 }) {
-  React.useEffect(() => {
-    console.log("map", stakeholders);
-  }, [stakeholders]);
+  // React.useEffect(() => {
+  //   if (stakeholders && stakeholders.length) {
+  //     stakeholders.forEach(stakeholder => console.log(stakeholder.name, stakeholder.hours))
+  //   }
+  //   console.log("map", stakeholders);
+  // }, [stakeholders]);
 
   const categoryIdsOrDefault = categoryIds.length
     ? categoryIds

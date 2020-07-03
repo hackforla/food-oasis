34af3da950d048999d4f6d639bb4bf243cad3b19
Tim Malstead <timmmalstead@gmail.com>
2020-07-02 19:18:00 -0700

    fixes header button issue and fixes footer link issue

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -87,8 +87,9 @@ const Footer = () => {
 
   const constantLinks = MENU_ITEMS.map((item, index) => {
     const { text, link } = item;
+    console.log(link);
     return (
-      <Link className={classes.linkStyle} key={index} href={link}>
+      <Link className={classes.linkStyle} key={index} to={link}>
         {text}
       </Link>
     );

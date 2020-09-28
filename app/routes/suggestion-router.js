89916dce036fd2e5a04dc7c3ca2065639f06afaa
stdyrm <17044124+stdyrm@users.noreply.github.com>
2020-09-27 14:35:38 -1000

    Added routes/services to request suggestions by user id (data entry users) and to request all/remove suggestions (admin users).

--- app/routes/suggestion-router.js
+++ app/routes/suggestion-router.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
 const suggestionController = require("../controllers/suggestion-controller");
+const jwtSession = require("../../middleware/jwt-session");
 
-router.get("/", suggestionController.getAll);
-router.get("/:id", suggestionController.getById);
+router.get(
+  "/",
+  jwtSession.validateUserHasRequiredRoles(["admin", "security_admin"]),
+  suggestionController.getAll
+);
+
+router.get("/:id", jwtSession.validateUser, suggestionController.getById);
+
+router.get(
+  "/assigned/:userId",
+  jwtSession.validateUser,
+  suggestionController.getAllByAssignedUser
+);
+
 router.post("/", suggestionController.post);
-// router.put("/:id", suggestionController.put);
+
+router.delete(
+  "/:id",
+  jwtSession.validateUserHasRequiredRoles(["admin", "security_admin"]),
+  suggestionController.remove
+);
 
 module.exports = router;

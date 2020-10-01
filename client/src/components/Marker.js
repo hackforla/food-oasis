c20c409be0b1869d78d67e61826f90602629da94
Brent Bovenzi <brent.bovenzi@gmail.com>
2020-10-01 14:40:48 -0500

    drop shadow
    - add a shadow and increase the zindex of a selected location

--- client/src/components/Marker.js
+++ client/src/components/Marker.js
@@ -1,12 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Marker } from "react-map-gl";
+import { makeStyles } from "@material-ui/core/styles";
 
 import mapMarker from "images/mapMarker";
 import {
   MEAL_PROGRAM_CATEGORY_ID,
   FOOD_PANTRY_CATEGORY_ID,
 } from "constants/stakeholder";
+
+const useStyles = makeStyles(() => ({
+  active: {
+    zIndex: 1,
+  },
+}));
 
 const MapMarker = ({ onClick, stakeholder, selectedStakeholder }) => {
   const {
@@ -16,8 +23,15 @@ const MapMarker = ({ onClick, stakeholder, selectedStakeholder }) => {
     inactive,
     inactiveTemporary,
   } = stakeholder;
+  const classes = useStyles();
+  const selected =
+    selectedStakeholder && selectedStakeholder.id === stakeholder.id;
   return (
-    <Marker longitude={longitude} latitude={latitude}>
+    <Marker
+      longitude={longitude}
+      latitude={latitude}
+      className={selected ? classes.active : ""}
+    >
       {mapMarker(
         categories[0]?.id === FOOD_PANTRY_CATEGORY_ID &&
           categories[1]?.id === MEAL_PROGRAM_CATEGORY_ID
@@ -27,7 +41,7 @@ const MapMarker = ({ onClick, stakeholder, selectedStakeholder }) => {
           : 1,
         inactiveTemporary || inactive,
         onClick,
-        selectedStakeholder && selectedStakeholder.id === stakeholder.id
+        selected
       )}
     </Marker>
   );

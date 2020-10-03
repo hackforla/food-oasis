e6bdbb748fdd07ab55124d8496f3a46721fb5370
stdyrm <17044124+stdyrm@users.noreply.github.com>
2020-10-03 13:46:10 -1000

    Replaced internal stakeholder state with parent component props to re-render stakeholder info in dialog after new markers are clicked

--- client/src/components/SuggestionDialog.js
+++ client/src/components/SuggestionDialog.js
@@ -14,87 +14,34 @@ import * as suggestionService from "../services/suggestion-service";
 
 function SuggestionDialog(props) {
   const { onClose, open, setToast, stakeholder: sh, ...other } = props;
-  const [stakeholder] = useState(
-    sh
-      ? {
-          id: sh.id,
-          name: sh.name,
-          address1: sh.address1,
-          address2: sh.address2,
-          city: sh.city,
-          state: sh.state,
-          zip: sh.zip,
-          phone: sh.phone,
-          email: sh.email,
-          notes: "",
-          hours: "",
-          tipsterName: "",
-          tipsterPhone: "",
-          tipsterEmail: "",
-          category: "",
-        }
-      : {
-          id: 0,
-          name: "",
-          address1: "",
-          address2: "",
-          city: "",
-          state: "",
-          zip: "",
-          phone: "",
-          email: "",
-          notes: "",
-          hours: "",
-          tipsterName: "",
-          tipsterPhone: "",
-          tipsterEmail: "",
-          category: "",
-        }
-  );
-  const [corrections, setCorrections] = useState(
-    sh
-      ? {
-          id: sh.id,
-          name: sh.name,
-          address1: "",
-          address2: "",
-          city: "",
-          state: "",
-          zip: "",
-          phone: "",
-          email: "",
-          notes: "",
-          hours: "",
-          tipsterName: "",
-          tipsterPhone: "",
-          tipsterEmail: "",
-          category: "",
-        }
-      : {
-          id: 0,
-          name: "",
-          address1: "",
-          address2: "",
-          city: "",
-          state: "",
-          zip: "",
-          phone: "",
-          email: "",
-          notes: "",
-          hours: "",
-          tipsterName: "",
-          tipsterPhone: "",
-          tipsterEmail: "",
-          category: "",
-        }
-  );
+
+  const [corrections, setCorrections] = useState({
+    name: "",
+    address1: "",
+    address2: "",
+    city: "",
+    state: "",
+    zip: "",
+    phone: "",
+    email: "",
+    notes: "",
+    hours: "",
+    tipsterName: "",
+    tipsterPhone: "",
+    tipsterEmail: "",
+    category: "",
+  });
 
   const handleCancel = () => {
     onClose(false);
   };
 
   const handleChange = (e) => {
-    setCorrections({ ...corrections, [e.target.name]: e.target.value });
+    setCorrections({
+      ...corrections,
+      id: sh ? sh.id : 0,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const handleSubmit = () => {
@@ -116,27 +63,19 @@ function SuggestionDialog(props) {
 
   return (
     <Dialog
-      disableBackdropClick
       disableEscapeKeyDown
       fullWidth
       maxWidth="sm"
       aria-labelledby="confirmation-dialog-title"
       open={open}
+      onClose={handleCancel}
       {...other}
     >
       <DialogTitle id="confirmation-dialog-title">Send Correction</DialogTitle>
       <DialogContent dividers>
         <Grid container spacing={1}>
           <Grid item xs={12}>
-            {/* <Typography>
-              Please help us improve our data by letting us know when our
-              information is incorrect. All fields are optional, but filling in
-              as many as you can helps our volunteers to validate efficiently.
-              Thanks!
-            </Typography> */}
-            <Typography variant="h5">
-              Organization: {stakeholder.name}
-            </Typography>
+            <Typography variant="h5">Organization: {sh.name}</Typography>
             <Typography>
               Please help us improve our data by letting us know when our
               information is incorrect. Please fill corrections into any of the
@@ -147,7 +86,7 @@ function SuggestionDialog(props) {
             <TextField
               type="text"
               size="small"
-              label={`Current name: ${stakeholder.name}`}
+              label={`Current name: ${sh.name}`}
               name="name"
               variant="outlined"
               margin="normal"
@@ -161,7 +100,7 @@ function SuggestionDialog(props) {
             <TextField
               type="text"
               size="small"
-              label={`Current address 1: ${stakeholder.address1}`}
+              label={`Current address 1: ${sh.address1}`}
               name="address1"
               variant="outlined"
               margin="normal"
@@ -175,7 +114,7 @@ function SuggestionDialog(props) {
             <TextField
               type="text"
               size="small"
-              label={`Current address 2: ${stakeholder.address2}`}
+              label={`Current address 2: ${sh.address2}`}
               name="address2"
               variant="outlined"
               margin="normal"
@@ -189,7 +128,7 @@ function SuggestionDialog(props) {
             <TextField
               type="text"
               size="small"
-              label={`Current city: ${stakeholder.city}`}
+              label={`Current city: ${sh.city}`}
               name="city"
               variant="outlined"
               margin="normal"
@@ -203,7 +142,7 @@ function SuggestionDialog(props) {
             <TextField
               type="text"
               size="small"
-              label={`Current state: ${stakeholder.state}`}
+              label={`Current state: ${sh.state}`}
               name="state"
               variant="outlined"
               margin="normal"
@@ -217,7 +156,7 @@ function SuggestionDialog(props) {
             <TextField
               type="text"
               size="small"
-              label={`Current zip code: ${stakeholder.zip}`}
+              label={`Current zip code: ${sh.zip}`}
               name="zip"
               variant="outlined"
               margin="normal"
@@ -231,7 +170,7 @@ function SuggestionDialog(props) {
             <TextField
               type="text"
               size="small"
-              label={`Current phone: ${stakeholder.phone}`}
+              label={`Current phone: ${sh.phone}`}
               name="phone"
               variant="outlined"
               margin="normal"
@@ -245,7 +184,7 @@ function SuggestionDialog(props) {
             <TextField
               type="text"
               size="small"
-              label={`Current email: ${stakeholder.email}`}
+              label={`Current email: ${sh.email}`}
               name="email"
               variant="outlined"
               margin="normal"

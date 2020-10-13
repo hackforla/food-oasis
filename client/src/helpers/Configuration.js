463ed267d85cf17c03706c5d2a896528bd6b465e
John Darragh <darragh@entrotech.net>
2020-10-13 14:47:25 -0700

    First pass at adding Hawaii tenant

--- client/src/helpers/Configuration.js
+++ client/src/helpers/Configuration.js
@@ -1,6 +1,21 @@
 export const getTenantId = () =>
-  window.location.hostname.toLowerCase().includes("ca.food") ||
-  window.location.hostname.toLowerCase().includes("california") ||
-  process.env.REACT_APP_TENANT_ID === "2"
+  window.location.hostname.toLowerCase().includes("hi.food") ||
+  window.location.hostname.toLowerCase().includes("hawaii") ||
+  process.env.REACT_APP_TENANT_ID === "3"
+    ? 3
+    : window.location.hostname.toLowerCase().includes("ca.food") ||
+      window.location.hostname.toLowerCase().includes("california") ||
+      process.env.REACT_APP_TENANT_ID === "2"
     ? 2
     : 1;
+
+export const defaultCoordinates = (() => {
+  switch (getTenantId()) {
+    case 3:
+      return { lat: 21.33629, lon: -157.89435 };
+    case 2:
+      return { lat: 38.576711, lon: -121.493671 };
+    default:
+      return { lat: 34.07872, lon: -118.243328 };
+  }
+})();

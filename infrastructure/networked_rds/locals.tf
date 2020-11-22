4266e445435bbf07b3997f6f04e4927fabccb2e0
Prashant Singh <singh.prasha@husky.neu.edu>
2020-11-21 23:47:06 -0500

    changes in the task and main.tf

--- infrastructure/networked_rds/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  bastion_name = "bastion-${var.account_id}-${local.namespace}-${var.stage}"
-  vpc_name     = "main-vpc"
-  namespace    = substr(var.project_name, 0, 6)
-  db_name      = "${var.project_name}-db"
-}

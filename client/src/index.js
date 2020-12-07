8b9e560c93e4eda1855e2caebeaa8b7dae00ad2e
John Darragh <darragh@entrotech.net>
2020-12-06 17:22:07 -0800

    Implement custome GTM events

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,20 @@ import ReactDOM from "react-dom";
 import "./i18n";
 import "styles/root.css";
 import App from "./App";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-PS74HS2",
+  events: {
+    customEvent: "customEvent",
+    viewDetail: null,
+    getDirections: null,
+    dialPhone: null,
+  },
+  action: null,
+  value: null,
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(<App />, document.getElementById("root"));

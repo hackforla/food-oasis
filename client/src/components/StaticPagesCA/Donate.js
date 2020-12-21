52fe64c53a011fc8ee3d0b8dac9bae274e866390
John Darragh <darragh@entrotech.net>
2020-12-21 15:14:53 -0800

    Change donate links back to Code for America

--- client/src/components/StaticPagesCA/Donate.js
+++ client/src/components/StaticPagesCA/Donate.js
@@ -179,8 +179,7 @@ const Donate = () => {
             Your donation will get earmarked for Food Oasis.
           </p>
           <a
-            // href="//www.codeforamerica.org/donate"
-            href="https://svbinthecommunity.benevity.org/community/fundraiser/1842"
+            href="//www.codeforamerica.org/donate"
             target="_blank"
             rel="noopener noreferrer"
             className={classes.btnOutline}

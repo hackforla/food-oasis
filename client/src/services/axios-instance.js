0053a06a88b838315c17cfd914e427a4001ae404
John Darragh <darragh@entrotech.net>
2020-12-24 17:15:45 -0800

    Modify axios instance to tolerate missing REACTACT_APP_WEB_API setting

--- client/src/services/axios-instance.js
+++ client/src/services/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}`,
+  baseURL: process.env.REACT_APP_API_URL || null,
   withCredentials: true,
 });
 

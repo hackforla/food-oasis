12081ea0b9c4a975aeb9a87a71ba49ec4edeaec8
Darren Pham <phamdarren1@gmail.com>
2021-02-28 18:50:08 -0800

    AWS deployment testing

--- infrastructure/templates/test.sh
+++ infrastructure/templates/test.sh
@@ -2,8 +2,11 @@
 # .containerDefinitions.image
 # .revision
 # .status
+# .registeredBy
+# .registeredAt
+
 
 aws ecs describe-task-definition \
   --task-definition foodoasis-dev-td \
   --query taskDefinition | \
-  jq 'del(.taskDefinitionArn,.revision,.status)'
+  jq 'del(.taskDefinitionArn,.revision,.status,.registeredAt,.registeredBy)'

d1abb62448c0e31dbf5b8049acc1a7411b2b5e11
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2021-04-29 21:17:12 -0700

    Add tenantId to parent org

---
+++ server/migrations/1619585842489_add-tenantId-to-parent-org.js
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.sql(
+    `ALTER TABLE public.parent_organization
+    ADD COLUMN "tenant_id" integer;`
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.sql(
+    `ALTER TABLE public.parent_organization
+      DROP COLUMN "tenant_id" integer;`
+  );
+};

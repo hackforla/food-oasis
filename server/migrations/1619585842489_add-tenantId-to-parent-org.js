a0409d86a2c13f269ce703caa3fab3e1fe691289
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2021-05-18 22:19:44 -0700

    Remove quotes from tenant_id

--- server/migrations/1619585842489_add-tenantId-to-parent-org.js
+++ server/migrations/1619585842489_add-tenantId-to-parent-org.js
@@ -5,13 +5,13 @@ exports.shorthands = undefined;
 exports.up = (pgm) => {
   pgm.sql(
     `ALTER TABLE public.parent_organization
-    ADD COLUMN "tenant_id" integer;`
+    ADD COLUMN tenant_id integer;`
   );
 };
 
 exports.down = (pgm) => {
   pgm.sql(
     `ALTER TABLE public.parent_organization
-      DROP COLUMN "tenant_id" integer;`
+      DROP COLUMN tenant_id integer;`
   );
 };

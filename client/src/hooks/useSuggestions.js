3149f065b4be440f62b3a32b0336e4b5851cc784
Virginia Wu <virginiawu11@gmail.com>
2021-10-09 15:03:32 -0400

    combine import React statements

--- client/src/hooks/useSuggestions.js
+++ client/src/hooks/useSuggestions.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import * as suggestionService from "../services/suggestion-service";
 
 export const useSuggestions = (params = {}) => {

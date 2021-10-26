6e413efd35e3deb2ffca652740d51ed5ee06479a
John Darragh <darragh@entrotech.net>
2021-10-25 17:45:06 -0700

    Try to fix package-lock files, restore Buttons.js
    Remove log

--- client/src/components/FoodSeeker/Home.js
+++ client/src/components/FoodSeeker/Home.js
@@ -160,8 +160,7 @@ const Home = ({
     let originCoordinates = { latitude: null, longitude: null };
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        (position) => { 
-          console.log({position})
+        (position) => {
           if (position) {
             originCoordinates = {
               latitude: position.coords.latitude,

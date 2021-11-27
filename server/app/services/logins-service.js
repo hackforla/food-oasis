08e8cd949e71b812a13c19be17837e433d77d21c
Virginia Wu <virginiawu11@gmail.com>
2021-11-27 13:42:35 -0500

    add logins id and order by most recent

--- server/app/services/logins-service.js
+++ server/app/services/logins-service.js
@@ -10,11 +10,12 @@ const insert = async (login_id, tenant_id) => {
 };
 
 const selectAll = async (tenantId) => {
-  const sql = `select login.first_name, login.last_name, login.email, logins.login_time
+  const sql = `select logins.id, login.first_name, login.last_name, login.email, logins.login_time
     from logins
     join login
     on login.id = logins.login_id
-    where logins.tenant_id = $<tenantId>`;
+    where logins.tenant_id = $<tenantId>
+    order by logins.login_time desc`;
 
   const rows = await db.any(sql, { tenantId });
   return camelcaseKeys(rows);

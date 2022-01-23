31546deae5e1656767f7c44a332978cc6ec38443
Virginia Wu <virginiawu11@gmail.com>
2022-01-23 14:39:10 -0500

    add toast message for when user is redirected

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,12 +1,22 @@
 import { Route, Redirect } from "react-router-dom";
 import { useUserContext } from "./contexts/user-context";
 import { useLocation } from "react-router-dom";
+import { useToasterContext } from "./contexts/toaster-context";
 
 function PrivateRoute({ path, children, roles }) {
   const { user } = useUserContext();
-  let location = useLocation();
+  const { setToast } = useToasterContext();
+  const location = useLocation();
 
   if (!user || !roles.some((role) => user[role])) {
+    const rolesStr = roles
+      .slice(0, roles.length - 1)
+      .map((role) => role.slice(2));
+    setToast({
+      message: `Please login to ${rolesStr} or ${roles[roles.length - 1].slice(
+        2
+      )} roles to access ${location.pathname}`,
+    });
     return (
       <Redirect
         to={{ pathname: "/login", state: { from: location.pathname } }}

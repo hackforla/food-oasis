d5ea5cf452c260be9e091a878ed7119b0c70e1bc
lodenrogue <phyzix5761@gmail.com>
2022-07-14 19:19:21 -0400

    Changed order of items

--- client/src/helpers/Constants.js
+++ client/src/helpers/Constants.js
@@ -1,6 +1,6 @@
 export const MENU_ITEMS = [
-  { text: "About Us", link: "/about" },
   { text: "Find Food", link: "/organizations" },
+  { text: "About Us", link: "/about" },
   { text: "Donate", link: "/donate" },
   { text: "FAQs", link: "/faqs" },
   { text: "Suggest New Listing", link: "/suggestion" },

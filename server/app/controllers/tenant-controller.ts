789a7785c1d3f1ce8b50da10af5673bc508f5fa2
Virginia Wu <virginiawu11@gmail.com>
2022-08-27 17:13:51 -0400

    add types to tenants router-controller-service

--- server/app/controllers/tenant-controller.ts
+++ server/app/controllers/tenant-controller.ts
@@ -1,6 +1,13 @@
-const tenantService = require("../services/tenant-service");
+import tenantService from "../services/tenant-service";
+import { RequestHandler, Response } from "express";
+import { Tenant } from "../types/tenant-types";
 
-const getAll = async (req, res) => {
+const getAll: RequestHandler<
+  never,
+  Tenant[],
+  never,
+  { searchParams: string }
+> = async (req, res) => {
   try {
     const resp = await tenantService.selectAll();
     res.send(resp);
@@ -10,12 +17,15 @@ const getAll = async (req, res) => {
   }
 };
 
-const getById = async (req, res) => {
+const getById: RequestHandler<{ id: string }, Tenant, never, never> = async (
+  req,
+  res
+) => {
   try {
-    const id = Number(req.params.id);
+    const id = req.params.id;
     const resp = await tenantService.selectById(id);
     res.send(resp);
-  } catch (err) {
+  } catch (err: any) {
     if (err.code === 0) {
       res.sendStatus(404);
     } else {
@@ -25,12 +35,15 @@ const getById = async (req, res) => {
   }
 };
 
-const post = async (req, res) => {
+const post: RequestHandler<never, { error: string }, Tenant, never> = async (
+  req,
+  res
+) => {
   try {
     await tenantService.insert(req.body);
     res.sendStatus(201);
   } catch (err) {
-    if (err.message.includes("duplicate")) {
+    if (err instanceof Error && err.message.includes("duplicate")) {
       res.status(400).json({ error: "Cannot insert duplicate row." });
     } else {
       console.error(err);
@@ -39,7 +52,12 @@ const post = async (req, res) => {
   }
 };
 
-const put = async (req, res) => {
+const put: RequestHandler<
+  { id: string },
+  { error: string },
+  Tenant,
+  never
+> = async (req, res) => {
   try {
     if (Number(req.params.id) !== req.body.id) {
       res.status(400).json({
@@ -59,12 +77,17 @@ const put = async (req, res) => {
   }
 };
 
-const remove = async (req, res) => {
+const remove: RequestHandler<
+  { id: string },
+  { error: string },
+  never,
+  never
+> = async (req, res) => {
   try {
     // params are always strings, need to
     // convert to correct Javascript type, so
     // pg=promise can format SQL correctly.
-    const id = Number(req.params.id);
+    const id = req.params.id;
     const rowCount = await tenantService.remove(id);
     if (rowCount !== 1) {
       res.status(400).json({ error: "Record not found" });
@@ -77,7 +100,7 @@ const remove = async (req, res) => {
   }
 };
 
-module.exports = {
+export default {
   getAll,
   getById,
   post,

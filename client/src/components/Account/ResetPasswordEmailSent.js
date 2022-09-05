4447a7f8187aa94075b18cb7ffd7f680a85642b2
Virginia Wu <virginiawu11@gmail.com>
2022-09-04 22:23:43 -0400

    update to mail icon

--- client/src/components/Account/ResetPasswordEmailSent.js
+++ client/src/components/Account/ResetPasswordEmailSent.js
@@ -6,6 +6,29 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Footer from "../Layout/Footer";
 import { Button } from "../../components/UI";
 
+const MailIcon = () => {
+  return (
+    <svg
+      width="27"
+      height="15"
+      viewBox="0 0 27 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6.63424 14H23.7009C24.8055 14 25.7009 13.1046 25.7009 12V3C25.7009 1.89543 24.8055 1 23.7009 1H7.39551C6.90286 1 6.70713 1.63726 7.11484 1.9138L14.8251 7.14337C15.146 7.36102 15.5638 7.37355 15.8971 7.17552L24.8342 1.86667"
+        stroke="white"
+        stroke-linecap="round"
+      />
+      <path
+        d="M8.8 5.7666H1M8.8 10.0999H4.03333"
+        stroke="white"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
 const styles = (theme) => ({
   "@global": {
     body: {
@@ -20,7 +43,7 @@ const styles = (theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.success.main,
   },
   header: {
     width: "100%", // Fix IE 11 issue.
@@ -48,7 +71,7 @@ const ResetPasswordEmailSent = (props) => {
         <CssBaseline />
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
+            <MailIcon />
           </Avatar>
           <Typography
             component="h1"

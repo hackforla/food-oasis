d539a04a2ffb7a6b135bffad0f9c00fcfaeaca94
John Darragh <darragh@entrotech.net>
2022-09-14 17:06:02 -0700

    Convert Neighborood endpoints to typescript

--- client/src/services/neighborhood-service.js
+++ client/src/services/neighborhood-service.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { tenantId } from "helpers/Configuration";
 
 const baseUrl = "/api/neighborhoods";
 
 export const getAll = async (searchParams) => {
-  searchParams = searchParams || {};
+  searchParams = { ...searchParams, tenantId } || { tenantId };
   try {
     const response = await axios.get(baseUrl, {
       params: searchParams,
@@ -20,6 +21,19 @@ export const getGeoJSONById = async (id) => {
       id,
     });
     return response.data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+// Not used yet, but will be used by admin screen for maintaining zoom levels
+// for neighborhoodss
+export const updateZoom = async (ncId, zoom) => {
+  try {
+    await axios.put(baseUrl + `/${ncId}`, {
+      ncId,
+      zoom,
+    });
   } catch (err) {
     throw new Error(err.message);
   }

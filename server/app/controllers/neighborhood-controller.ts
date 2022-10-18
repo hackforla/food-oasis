0b0e8bab42edd072f6af6ba9d5ec7ee2aaefe57b
John Darragh <darragh@entrotech.net>
2022-10-17 19:21:19 -0700

    Convert Neighborood endpoints to typescript (#1340)

---
+++ server/app/controllers/neighborhood-controller.ts
@@ -0,0 +1,69 @@
+import neighborhoodService from "../services/neighborhood-service";
+import { RequestHandler, Response } from "express";
+import {
+  Neighborhood,
+  NeighborhoodGeoJSON,
+  NeighborhoodPutRequest,
+} from "../types/neighborhood-types";
+
+const getAll: RequestHandler<
+  { tenantId: string },
+  Neighborhood[] | { error: string },
+  never,
+  never
+> = (req, res) => {
+  const { tenantId } = req.query;
+  neighborhoodService
+    .selectAll(Number(tenantId || 1))
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((err) => {
+      res.status(404).json({ error: err.toString() });
+    });
+};
+
+const getGeoJSONById: RequestHandler<
+  { ncId: number },
+  NeighborhoodGeoJSON | { error: string },
+  never,
+  never
+> = (req, res) => {
+  const { ncId } = req.params;
+  neighborhoodService
+    .selectGeoJSONById(Number(ncId))
+    .then((resp) => {
+      res.send(resp);
+    })
+    .catch((err) => {
+      res.status(404).json({ error: err.toString() });
+    });
+};
+
+const updateZoom: RequestHandler<
+  { ncId: number },
+  never,
+  { ncId: number; zoom: number },
+  never
+> = (req, res) => {
+  const { ncId, zoom } = req.body;
+  const { ncId: ncIdParam } = req.params;
+  if (Number(ncIdParam) !== ncId) {
+    res.sendStatus(400);
+  }
+
+  neighborhoodService
+    .updateZoom(ncId, zoom)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((_err) => {
+      res.status(500);
+    });
+};
+
+module.exports = {
+  getAll,
+  getGeoJSONById,
+  updateZoom,
+};
---
+++ server/app/types/neighborhood-types.ts
@@ -0,0 +1,33 @@
+export interface Neighborhood {
+  id: number;
+  name: string;
+  website: string;
+  empowerLink: string;
+  ncId: number;
+  certified: string;
+  serviceRegion: string;
+  zoom: number;
+}
+
+export interface NeighborhoodGeoJSON extends Neighborhood {
+  centroidLongitude: number;
+  centroidLatitude: number;
+  geojson: GeoJSON;
+}
+
+export interface NeighborhoodPutRequest {
+  ncId: number;
+  zoom: number;
+}
+
+export interface GeoJSON {
+  id: number;
+  name: string;
+  type: string; // For Neighborhoods, this has the value "Feature"
+  geometry: Geometry;
+}
+
+export interface Geometry {
+  type: string; // for Neighborhoods, this has the value "MultiPolygon"
+  coordinates: number[][][][];
+}

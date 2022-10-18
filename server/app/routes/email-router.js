fa0baca71f190dc15582cfdd023d9f30a2176564
Neck <66407817+Nechir-89@users.noreply.github.com>
2022-10-17 22:25:01 -0400

    1248 add types email (#1352)
    * add typescript to email-router
    
    * add email-types
    
    * add typescript to email-controller
    
    * add type to sendgrid-service
    
    * code formating

--- server/app/routes/email-router.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const emailController = require("../controllers/email-controller");
-
-router.post("/", emailController.send);
-
-module.exports = router;

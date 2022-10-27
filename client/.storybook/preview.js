0aa85a5c5e46641957ced5d95f924e5d7741fb30
Nikesh Shrestha <nikesh123u@gmail.com>
2022-10-27 21:40:09 +0545

    Remove StoryBook from the project

--- client/.storybook/preview.js
+++
@@ -1,8 +0,0 @@
-import { muiTheme } from "storybook-addon-material-ui";
-import { theme as adminTheme } from "../src/theme/adminTheme.js";
-import clientTheme from "../src/theme/clientTheme.js";
-
-export const decorators = [muiTheme([adminTheme, clientTheme])];
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-};

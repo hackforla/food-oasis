d0f79ff29a97c92d678a8d9083445ae531649276
Qiqi Zheng <68657634+qiqicodes@users.noreply.github.com>
2022-10-29 20:32:50 -0500

    add types to faq files (#1410)

--- server/app/services/faq-service.ts
+++ server/app/services/faq-service.ts
@@ -1,42 +1,45 @@
-const db = require("./db");
-const camelcaseKeys = require("camelcase-keys");
+import db from "./db";
+import camelcaseKeys from "camelcase-keys";
+import { Faq } from "../types/faq-types";
 
 const selectAll = async () => {
   const sql = `
     select id, question, answer, language, identifier
     from faq
   `;
-  const result = await db.manyOrNone(sql);
+  const result: Faq[] = await db.manyOrNone(sql);
   return result.map((r) => camelcaseKeys(r));
 };
 
 // Identifier Table in DB follows `#:identifier` scheme, for both identifying and also ordering
-const selectAllByLanguage = async (language) => {
+const selectAllByLanguage = async (language: string) => {
   const sql = `
     select id, question, answer, language, identifier
     from faq
     where language = $<language>
     order by identifier desc
   `;
-  const result = await db.manyOrNone(sql, { language });
+  const result: Faq[] = await db.manyOrNone(sql, { language });
   return result.map((r) => camelcaseKeys(r));
 };
 
-const selectById = async (faqId) => {
+const selectById = async (faqId: string) => {
   // Need to cast id to number so pg-promise knows how
   // to format SQL
   const id = Number(faqId);
   const sql = `select id, question, answer, language, identifier 
   from faq where id = $<id>`;
 
-  const row = await db.one(sql, { id });
+  const row: Faq = await db.one(sql, { id });
   return camelcaseKeys(row);
 };
 
-const selectByIdentifier = async (identifier) => {
+const selectByIdentifier = async (identifier: string) => {
   const sql = `select id, question, answer, language, identifier 
   from faq where identifier = $1`;
-  const result = await db.manyOrNone(sql, { identifier: Number(identifier) });
+  const result: Faq[] = await db.manyOrNone(sql, {
+    identifier: Number(identifier),
+  });
   return result.map((r) => camelcaseKeys(r));
 };
 
@@ -46,12 +49,12 @@ ex. [{question: "", answer: "", language: "en", identifier: "example"}, {questio
 Do 2 requests to POST /api/faqs/
 */
 
-const insert = async (model) => {
+const insert = async (data: Faq) => {
   const sql = `insert into faq (question, answer, language, identifier) 
   values ($<question>, $<answer>, $<language>, $<identifier>) 
   returning id`;
 
-  const result = await db.one(sql, model);
+  const result: Faq = await db.one(sql, data);
   return { id: result.id };
 };
 
@@ -61,13 +64,13 @@ ex. [{question: "", answer: "", language: "en"}, {question: "", answer: "", lang
 Do 2 requests to PUT /api/faqs/
 */
 
-const update = async (model) => {
+const update = async (id: string, data: Faq) => {
   const sql = `
     update faq
     set question = $1, answer = $2, language = $3, identifier = $4
     where id = $5 returning *
   `;
-  return await db.one(sql, model);
+  return await db.one(sql, { ...data, id: Number(id) });
 };
 
 /*
@@ -75,12 +78,12 @@ Deletes all FAQs from identifier
 ex. {identifier: ""}
 Verify that admin wants to delete FAQs, as it will delete all languages for FAQ.
 */
-const remove = async (identifier) => {
+const remove = async (identifier: string) => {
   const sql = `delete from faq where identifier = $<identifier>`;
   return await db.none(sql, { identifier: Number(identifier) });
 };
 
-module.exports = {
+export default {
   selectAll,
   selectAllByLanguage,
   selectById,

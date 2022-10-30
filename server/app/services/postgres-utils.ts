bd5c9eb7c691b8c9182e892a57a719e03fdaee7c
Qiqi Zheng <68657634+qiqicodes@users.noreply.github.com>
2022-10-29 20:31:14 -0500

    add types to postgress util (#1400)

--- server/app/services/postgres-utils.ts
+++ server/app/services/postgres-utils.ts
@@ -1,32 +1,26 @@
 // Module of utilties for working with PostgreSQL
 
 // Convert a javascript string to a string for postgres
-const toSqlString = (originalString) => {
+export const toSqlString = (originalString: string): string => {
   if (!originalString) {
     return "''";
   }
   return "'" + originalString.replace(/'/g, "''") + "'";
 };
 
-const toSqlNumeric = (originalNumeric) => {
+export const toSqlNumeric = (originalNumeric: number): string => {
   if (!originalNumeric) {
     return "null";
   }
   return originalNumeric.toString();
 };
 
-const toSqlTimestamp = (originalDatetime) => {
+export const toSqlTimestamp = (originalDatetime: Date): string => {
   if (!originalDatetime) {
     return "null";
   }
   return `'${originalDatetime.toString()}'`;
 };
 
-const toSqlBoolean = (originalBoolean) => (originalBoolean ? "true" : "false");
-
-module.exports = {
-  toSqlString,
-  toSqlNumeric,
-  toSqlBoolean,
-  toSqlTimestamp,
-};
+export const toSqlBoolean = (originalBoolean: boolean): string =>
+  originalBoolean ? "true" : "false";

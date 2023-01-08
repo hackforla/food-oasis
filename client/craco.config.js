7e103e27be7b3e42d7aeca3d95d338da7b90bf1c
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2023-01-08 15:27:11 -0800

    Lazy load components

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,7 +1,13 @@
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 module.exports = {
   babel: {
     loaderOptions: {
       ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"],
     },
+  },
+  webpack: {
+    plugins: [new BundleAnalyzerPlugin({ analyzerMode: "server" })],
   },
 };

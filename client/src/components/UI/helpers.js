181712f5633b6c1f786ff626b188dc6397641bec
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2023-01-08 15:39:08 -0800

    Remove storybook

--- client/src/components/UI/helpers.js
+++
@@ -1,5 +0,0 @@
-import { muiTheme } from "storybook-addon-material-ui";
-import { theme as adminTheme } from "../../theme/adminTheme";
-import clientTheme from "../../theme/clientTheme";
-
-export const decorators = muiTheme([adminTheme, clientTheme]);

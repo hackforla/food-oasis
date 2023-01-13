8405f10594f6c30e1ab7743180b5c8d3835a8e58
Matthew Bozin <mbozin94@gmail.com>
2023-01-13 11:29:41 -0800

    Create aws_service in client to call backend aws api. Modified LocationAutocomplete and SearchCriteria to accept new results data format.

--- client/src/services/aws_service.js
+++ client/src/services/aws_service.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 const baseUrl = "/api/aws";
 
-// This geocoder api is deprecated for the Food Oasis App,
-// as it was found to out of servcie for hours at a time.
-// Use the geocode-tamu-service.geocode function instead.
-
-export const geocode = async (address) => {
+export const autoComplete = async (address) => {
   const response = await axios.get(baseUrl + "/geocode", {
     params: { address },
   });

2c951cd858391a5b6020e0e9aab436f1914c8f52
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2023-01-17 21:23:04 -0800

    Remove webpack bundle analyzer

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,15 +1,7 @@
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
 module.exports = {
   babel: {
     loaderOptions: {
       ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"],
     },
-  },
-  webpack: {
-    plugins: process.env.NODE_ENV === "production" && [
-      new BundleAnalyzerPlugin({ analyzerMode: "server" }),
-    ],
   },
 };

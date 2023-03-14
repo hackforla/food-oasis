d345ae74eae84f6b19fc101d640b529104faa0a7
Matthew Bozin <mbozin94@gmail.com>
2023-03-13 19:07:44 -0700

    Added typescript fixes for lint errors in aws-service.

--- server/app/services/aws-service.ts
+++ server/app/services/aws-service.ts
@@ -1,9 +1,10 @@
 import AWS from "aws-sdk"
 
 const generateLocation = async () => {
+    if (!process.env.IDENTITY_POOL_ID) return;
     AWS.config.update({ region: 'us-west-2' });
-    const poolId: any = process.env.IDENTITY_POOL_ID;
-    const credentials: any = new AWS.CognitoIdentityCredentials({
+    const poolId: string = process.env.IDENTITY_POOL_ID;
+    const credentials: AWS.CognitoIdentityCredentials = new AWS.CognitoIdentityCredentials({
         IdentityPoolId: poolId,
     })
     const location = new AWS.Location({
@@ -14,32 +15,40 @@ const generateLocation = async () => {
 }
 
 const autocomplete = async (address: string) => {
+    if (!process.env.PLACE_INDEX_NAME) return;
     const location = await generateLocation()
-    const params: any = {
+    const params: AWS.Location.SearchPlaceIndexForTextRequest = {
         "IndexName": process.env.PLACE_INDEX_NAME,
         "Text": address,
         "MaxResults": 10,
         "FilterCountries": ["USA"],
         "Language": "en"
     };
+    if (!location) return;
     const data = await location.searchPlaceIndexForText(params).promise().then(results => results)
     data.Results.forEach((result) => {
-        result.Place.Geometry.Point?.push(result.Place.Geometry.Point.shift())
+        const point: number | undefined = data.Results[0].Place.Geometry.Point?.shift()
+        if (!point) return;
+        result.Place.Geometry.Point?.push(point)
     })
     return data
 }
 
 const getCoords = async (address: string) => {
+    if (!process.env.PLACE_INDEX_NAME) return;
     const location = await generateLocation()
-    const params: any = {
+    const params: AWS.Location.SearchPlaceIndexForTextRequest = {
         "IndexName": process.env.PLACE_INDEX_NAME,
         "Text": address,
         "MaxResults": 1,
         "FilterCountries": ["USA"],
         "Language": "en"
     };
+    if (!location) return;
     const data = await location.searchPlaceIndexForText(params).promise().then(results => results)
-    data.Results[0].Place.Geometry.Point?.push(data.Results[0].Place.Geometry.Point.shift())
+    const point: number | undefined = data.Results[0].Place.Geometry.Point?.shift()
+    if (!point) return;
+    data.Results[0].Place.Geometry.Point?.push(point)
     return data
 }
 

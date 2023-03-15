6596b503eb6b3d95c6e964c65c70bbf3b1ef6cf9
John Darragh <darragh@entrotech.net>
2023-03-15 10:57:24 -0700

    Fix MuiDemo page MuiLink section

--- client/src/components/MuiDemo/MuiLink.js
+++ client/src/components/MuiDemo/MuiLink.js
@@ -1,4 +1,4 @@
-import { Stack, Link } from "@mui/material";
+import { Box, Stack, Link, List, ListItem, Typography } from "@mui/material";
 
 function MuiLink() {
   return (
@@ -7,9 +7,24 @@ function MuiLink() {
         <Link href="http://hackforla.org">Hack for LA</Link>
         <Link href="http://nonexistentlink">Unvisitable Link</Link>
         <Link href="http://hackforla.org">Hack for LA</Link>
-        <Link href="http://hackforla.org" disabled>
-          Disabled Link
-        </Link>
+
+        <Box>
+          <Typography variant="body1" component={"p"}>
+            Possible states are:
+          </Typography>
+
+          <List>
+            <ListItem>normal (:link pseudo class)</ListItem>
+            <ListItem>visited (href appears in browser history)</ListItem>
+            <ListItem>focus (link has keyboard focus)</ListItem>
+            <ListItem>hover (mouse is hovering over link)</ListItem>
+            <ListItem>active (link is being pressed)</ListItem>
+          </List>
+          <p>
+            Only normal and visited are mutually exclusive. For example, a link
+            can be normal, focus and hover.
+          </p>
+        </Box>
       </Stack>
     </Stack>
   );

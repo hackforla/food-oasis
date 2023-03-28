6980cc098d8b167d0816455af5d9656520035ee0
June Zhao <bz.bijun@gmail.com>
2023-03-27 17:24:32 -0700

    Clean up

--- client/src/helpers/Configuration.js
+++ client/src/helpers/Configuration.js
@@ -6,5 +6,5 @@ export const tenantId = getTenantId();
 export const tenantName = TENANT_CONFIG[tenantId].tenantName;
 
 export const tenantDetails = TENANT_CONFIG[tenantId];
-console.log(tenantId, TENANT_CONFIG);
+
 export const defaultViewport = TENANT_CONFIG[tenantId].defaultViewport;

07c656754686eee6f11ee15ed1c80d32371eb4e4
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2023-04-01 13:30:35 -0400

    remove extra comments

--- server/middleware/request-validation-middlewares.ts
+++ server/middleware/request-validation-middlewares.ts
@@ -2,18 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import Ajv from 'ajv';
 const ajv = new Ajv({ allErrors: true });
 
-// export const validateTenantMiddleware: RequestHandler = (req, res, next) => {
-
-//   const valid = validateTenantRequest(req.body);
-//   if (!valid) {
-//     console.log(validateTenantRequest.errors);
-//     res.sendStatus(400);
-//   }
-//   else
-//     next();
-// }
-
-
 export const requestValidationMiddleware = (schema: any) => {
   
   const validatingRequest = ajv.compile(schema);

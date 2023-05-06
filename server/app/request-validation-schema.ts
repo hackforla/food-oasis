b54d6aacebb40c06b4290f55256b91534890bed8
Nechir-89 <nechirvan.abdullah@outlook.com>
2023-05-06 10:54:14 -0400

    add request validation to Tag POST request

--- server/app/request-validation-schema.ts
+++ server/app/request-validation-schema.ts
@@ -2,7 +2,6 @@ import { JSONSchemaType } from "ajv";
 import { Tenant } from '../types/tenant-types';
 import { StakeholderTag } from '../types/tag-types';
 
-
 export const tenantRequestSchema: JSONSchemaType<Tenant> = {
   type: "object",
   required: ["id", "name", "code"],
@@ -14,18 +13,19 @@ export const tenantRequestSchema: JSONSchemaType<Tenant> = {
     },
     name: {
       type: "string",
+      minLength: 1,
       maxLength: 256,
     },
     code: {
       type: "string",
+      minLength: 1,
       maxLength: 256,
     },
   },
   additionalProperties: false,
 };
 
-// This schema is based on put request for tag
-export const tagRequestSchema: JSONSchemaType<StakeholderTag> = {
+export const tagPutRequestSchema: JSONSchemaType<StakeholderTag> = {
   type: "object",
   required: ["id", "name", "tenantId"],
   properties: {
@@ -37,7 +37,7 @@ export const tagRequestSchema: JSONSchemaType<StakeholderTag> = {
     name: {
       type: "string",
       minLength: 1,
-      maxLength: 50,
+      maxLength: 256,
     },
     tenantId: {
       type: "integer",
@@ -47,3 +47,21 @@ export const tagRequestSchema: JSONSchemaType<StakeholderTag> = {
   },
   additionalProperties: false,
 };
+
+export const tagPostRequestSchema: JSONSchemaType<Omit<StakeholderTag, "id">> = {
+  type: "object",
+  required: ["name", "tenantId"],
+  properties: {
+    name: {
+      type: "string",
+      minLength: 1,
+      maxLength: 256
+    },
+    tenantId: {
+      type: "integer",
+      minimum: 1,
+      maximum: 10000,
+    },
+  },
+  additionalProperties: false
+}
--- server/app/routes/tag-router.ts
+++ server/app/routes/tag-router.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import * as tagController from "../controllers/tag-controller";
 import jwtSession from "../../middleware/jwt-session";
 import {requestValidationMiddleware} from '../../middleware/request-validation-middlewares'
-import { tagRequestSchema } from "../request-validation-schema";
+import { tagPutRequestSchema } from "../request-validation-schema";
+import { tagPostRequestSchema } from "../request-validation-schema";
 
 const router = Router();
 
@@ -11,13 +12,14 @@ router.get("/:tenantId", tagController.getAllByTenantId);
 router.post(
   "/",
   jwtSession.validateUserHasRequiredRoles(["admin"]),
+  requestValidationMiddleware(tagPostRequestSchema),
   tagController.post
 );
 
 router.put(
   "/:tagId",
   jwtSession.validateUserHasRequiredRoles(["admin"]),
-  requestValidationMiddleware(tagRequestSchema),
+  requestValidationMiddleware(tagPutRequestSchema),
   tagController.put
 );
 

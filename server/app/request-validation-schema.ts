be54c1cd8e166ef314e5283c7c54f6eacf583077
Nechir-89 <nechirvan.abdullah@outlook.com>
2023-05-06 16:48:01 -0400

    add API request validation for Suggestion PUT and POST requests

--- server/app/request-validation-schema.ts
+++ server/app/request-validation-schema.ts
@@ -1,6 +1,7 @@
 import { JSONSchemaType } from "ajv";
 import { Tenant } from '../types/tenant-types';
 import { StakeholderTag } from '../types/tag-types';
+import { Suggestion } from "../types/suggestion-types";
 
 export const tenantRequestSchema: JSONSchemaType<Tenant> = {
   type: "object",
@@ -65,3 +66,163 @@ export const tagPostRequestSchema: JSONSchemaType<Omit<StakeholderTag, "id">> = 
   },
   additionalProperties: false
 }
+
+export const suggestionPutRequestSchema: JSONSchemaType<Suggestion> = {
+  type: "object",
+  required: ["id", "adminNotes", "suggestionStatusId"],
+  properties: {
+    id: {
+      type: "integer",
+      minimum: 1,
+      maximum: 10000,
+    },
+    adminNotes: {
+      type: "string",
+      minLength: 1,
+      maxLength: 256,
+    },
+    suggestionStatusId: {
+      type: "integer",
+    },
+    name: {
+      type: "string",
+    },
+    address1: {
+      type: "string",
+    },
+    address2: {
+      type: "string",
+    },
+    city: {
+      type: "string",
+    },
+    state: {
+      type: "string",
+    },
+    zip: {
+      type: "string",
+    },
+    phone: {
+      type: "string",
+    },
+    email: {
+      type: "string",
+    },
+    notes: {
+      type: "string",
+    },
+    tipsterName: {
+      type: "string",
+    },
+    tipsterPhone: {
+      type: "string",
+    },
+    tipsterEmail: {
+      type: "string",
+    },
+    hours: {
+      type: "string",
+    },
+    category: {
+      type: "string",
+    },
+    stakeholderId: {
+      type: "number",
+    },
+    tenantId: {
+      type: "integer",
+    }
+  },
+  additionalProperties: false
+};
+
+export const suggestionPostRequestSchema: JSONSchemaType<Omit<Suggestion, "id">> = {
+  type: "object",
+  required: [
+    "stakeholderId", "adminNotes", "suggestionStatusId", "name", "address1", "address2", "city",
+    "state", "zip", "phone", "email", "notes", "tipsterName", "tipsterPhone", "tipsterEmail",
+    "hours", "category", "tenantId"
+  ],
+  properties: {
+    stakeholderId: {
+      type: "integer",
+      minimum: 1,
+      maximum: 10000,
+    },
+    adminNotes: {
+      type: "string",
+      maxLength: 256,
+    },
+    suggestionStatusId: {
+      type: "integer",
+      minimum: 1,
+      maximum: 100,
+    },
+    name: {
+      type: "string",
+      minLength: 1,
+      maxLength: 256,
+    },
+    address1: {
+      type: "string",
+      minLength: 1,
+      maxLength: 256,
+    },
+    address2: {
+      type: "string",
+      maxLength: 256,
+    },
+    city: {
+      type: "string",
+      minLength: 1,
+      maxLength: 256,
+    },
+    state: {
+      type: "string",
+      minLength: 1,
+      maxLength: 256,
+    },
+    zip: {
+      type: "string",
+      maxLength: 256,
+    },
+    phone: {
+      type: "string",
+      maxLength: 256,
+    },
+    email: {
+      type: "string",
+      maxLength: 256,
+    },
+    notes: {
+      type: "string",
+      maxLength: 1024,
+    },
+    tipsterName: {
+      type: "string",
+      maxLength: 256
+    },
+    tipsterPhone: {
+      type: "string",
+      maxLength: 256,
+    },
+    tipsterEmail: {
+      type: "string",
+      maxLength: 256,
+    },
+    hours: {
+      type: "string",
+      maxLength: 1024,
+    },
+    category: {
+      type: "string",
+      maxLength: 256,
+    },
+    tenantId: {
+      type: "integer",
+      minimum: 1,
+      maximum: 10000,
+    }
+  },
+  additionalProperties: false
+};
--- server/app/routes/suggestion-router.ts
+++ server/app/routes/suggestion-router.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 import suggestionController from "../controllers/suggestion-controller";
+import { suggestionPutRequestSchema, suggestionPostRequestSchema } from '../request-validation-schema'
+import { requestValidationMiddleware } from '../../middleware/request-validation-middlewares'
 
 router.get("/", suggestionController.getAll);
 router.get("/:id", suggestionController.getById);
-router.post("/", suggestionController.post);
-router.put("/:id", suggestionController.put);
+router.post("/", requestValidationMiddleware(suggestionPostRequestSchema), suggestionController.post);
+router.put("/:id", requestValidationMiddleware(suggestionPutRequestSchema), suggestionController.put);
 
 export default router;

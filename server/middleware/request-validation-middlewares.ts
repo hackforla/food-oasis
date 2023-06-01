54eeb1c193b0a736a50a598063eccb66e0b82253
Nechir-89 <nechirvan.abdullah@outlook.com>
2023-06-01 03:35:12 -0400

    install ajv-formats

--- server/middleware/request-validation-middlewares.ts
+++ server/middleware/request-validation-middlewares.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-import Ajv from 'ajv';
+import Ajv from "ajv";
+import addFormats from "ajv-formats"
 const ajv = new Ajv({ allErrors: true });
+addFormats(ajv);
 
 export function requestValidationMiddleware<T>(schema: T) {
 

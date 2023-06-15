c9d2771cea1f6b1c563584fa9ff932957c7d86a8
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-06-15 07:17:56 -0500

    only left to change svg icon, need other pr to be merged first

--- client/src/components/StaticPages/About.js
+++ client/src/components/StaticPages/About.js
@@ -47,95 +47,95 @@ const useStyles = makeStyles(() => ({
     margin: "auto",
     marginBottom: "20px",
   },
-  mission: {
-    padding: "50px 32px 50px",
-    margin: "32px 0",
-    borderRadius: "24px",
-    background: "#f0f0f0",
-    display: "flex",
-    flexDirection: "column",
-    "& $h2": {
-      flexBasis: "100",
-      textAlign: "center",
-      fontWeight: "500",
-      fontSize: "32px",
-      marginTop: "10px",
-      marginBottom: "20px",
-    },
-    "& $p": {
-      marginBottom: "16px",
-    },
-  },
-  team: {
-    padding: "50px 32px 50px",
-    margin: "32px 0 0 0",
-    borderRadius: "24px",
-    background: "#B6D8FB",
-    display: "flex",
-    flexDirection: "column",
-    "& $h2": {
-      flexBasis: "100",
-      textAlign: "center",
-      fontWeight: "500",
-      fontSize: "32px",
-      marginTop: "10px",
-      marginBottom: "20px",
-    },
-    "& $p": {
-      marginBottom: "16px",
-    },
-  },
-  contact: {
-    padding: "50px 32px 50px",
-    margin: "32px 0 0 0",
-    borderRadius: "24px",
-    background: "#f0f0f0",
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "center",
-    "& $h2": {
-      flexBasis: "100",
+  // mission: {
+  //   padding: "50px 32px 50px",
+  //   margin: "32px 0",
+  //   borderRadius: "24px",
+  //   background: "#f0f0f0",
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   "& $h2": {
+  //     flexBasis: "100",
+  //     textAlign: "center",
+  //     fontWeight: "500",
+  //     fontSize: "32px",
+  //     marginTop: "10px",
+  //     marginBottom: "20px",
+  //   },
+  //   "& $p": {
+  //     marginBottom: "16px",
+  //   },
+  // },
+  // team: {
+  //   padding: "50px 32px 50px",
+  //   margin: "32px 0 0 0",
+  //   borderRadius: "24px",
+  //   background: "#B6D8FB",
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   "& $h2": {
+  //     flexBasis: "100",
+  //     textAlign: "center",
+  //     fontWeight: "500",
+  //     fontSize: "32px",
+  //     marginTop: "10px",
+  //     marginBottom: "20px",
+  //   },
+  //   "& $p": {
+  //     marginBottom: "16px",
+  //   },
+  // },
+  // contact: {
+  //   padding: "50px 32px 50px",
+  //   margin: "32px 0 0 0",
+  //   borderRadius: "24px",
+  //   background: "#f0f0f0",
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   textAlign: "center",
+  //   "& $h2": {
+  //     flexBasis: "100",
 
-      textAlign: "center",
-      fontWeight: "500",
-      fontSize: "32px",
-      marginTop: "10px",
-      marginBottom: "20px",
-    },
-    "& $p": {
-      marginBottom: "16px",
-    },
-  },
-  partners: {
-    background: "#fff",
-    padding: "48px 32px",
-    borderRadius: "24px",
-    justifyContent: "space-between",
-    alignItems: "center",
-    display: "flex",
-    flexWrap: "wrap",
-    flexDirection: "column",
-    "& $h2": {
-      width: "100%",
-      flexBasis: "100",
-      textAlign: "center",
-      fontWeight: "500",
-      fontSize: "32px",
-      marginTop: "10px",
-      marginBottom: "60px",
-    },
-    "@media only screen and (min-width: 64em)": {
-      flexDirection: "row",
-    },
-  },
-  partnersLogo: {
-    maxWidth: "100%",
-    height: "100%",
-    marginBottom: "40px",
-    "@media only screen and (min-width: 64em) ": {
-      marginBottom: 0,
-    },
-  },
+  //     textAlign: "center",
+  //     fontWeight: "500",
+  //     fontSize: "32px",
+  //     marginTop: "10px",
+  //     marginBottom: "20px",
+  //   },
+  //   "& $p": {
+  //     marginBottom: "16px",
+  //   },
+  // },
+  // partners: {
+  //   background: "#fff",
+  //   padding: "48px 32px",
+  //   borderRadius: "24px",
+  //   justifyContent: "space-between",
+  //   alignItems: "center",
+  //   display: "flex",
+  //   flexWrap: "wrap",
+  //   flexDirection: "column",
+  //   "& $h2": {
+  //     width: "100%",
+  //     flexBasis: "100",
+  //     textAlign: "center",
+  //     fontWeight: "500",
+  //     fontSize: "32px",
+  //     marginTop: "10px",
+  //     marginBottom: "60px",
+  //   },
+  //   "@media only screen and (min-width: 64em)": {
+  //     flexDirection: "row",
+  //   },
+  // },
+  // partnersLogo: {
+  //   maxWidth: "100%",
+  //   height: "100%",
+  //   marginBottom: "40px",
+  //   "@media only screen and (min-width: 64em) ": {
+  //     marginBottom: 0,
+  //   },
+  // },
 }));
 
 const About = () => {
@@ -177,16 +177,36 @@ const About = () => {
           </Typography>{" "}
         </Container>
 
-        <div className={classes.mission}>
+        <Box
+        sx={{
+          padding: "50px 32px 50px",
+          margin: "32px 0",
+          borderRadius: "24px",
+          background: "#f0f0f0",
+          display: "flex",
+          flexDirection: "column"
+        }}
+        >
           <img
             alt="Our Mission"
             src={iconSpacerGray}
             className={classes.icon}
             height="40"
           />
-          <Typography variant="h2">Our Mission</Typography>
+          <Typography
+          sx={{
+            flexBasis: "100",
+            textAlign: "center",
+            marginTop: "10px",
+            marginBottom: "20px"
+          }}
+          variant="h2">Our Mission</Typography>
           <Container maxWidth="sm">
-            <Typography variant="body1">
+            <Typography
+            sx={{
+              marginBottom: "16px"
+            }}
+            variant="body1">
               Food Oasis is a web-based directory that connects people to free
               food resources in Los Angeles. Our team is dedicated to
               maintaining an updated directory of hundreds of free food
@@ -195,17 +215,36 @@ const About = () => {
               comprehensive and up-to-date information. 
             </Typography>
           </Container>
-        </div>
-        <Box className={classes.team}>
+        </Box>
+        <Box
+        sx={{
+          padding: "50px 32px 50px",
+          margin: "32px 0 0 0",
+          borderRadius: "24px",
+          background: "#B6D8FB",
+          display: "flex",
+          flexDirection: "column",
+        }}>
           <img
             alt="Our Team"
             src={iconSpacerGray}
             className={classes.icon}
             height="40"
           />
-          <Typography variant="h2">Our Team</Typography>
+          <Typography
+          sx={{
+            flexBasis: "100",
+            textAlign: "center",
+            marginTop: "10px",
+            marginBottom: "20px"
+          }}
+          variant="h2">Our Team</Typography>
           <Container maxWidth="sm">
-            <Typography variant="body1">
+            <Typography
+            sx={{
+              marginBottom: "16px"
+            }}
+            variant="body1">
               We are a 100% volunteer-run project. We are part of{" "}
               <Link
                 variant="primary"
@@ -223,15 +262,36 @@ const About = () => {
             </Typography>
           </Container>
         </Box>
-        <Box className={classes.contact}>
+        <Box
+        sx={{
+          padding: "50px 32px 50px",
+          margin: "32px 0 0 0",
+          borderRadius: "24px",
+          background: "#f0f0f0",
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center"
+        }}
+        >
           <img
             alt="Questions"
             src={iconSpacerGray}
             className={classes.icon}
             height="40"
           />
-          <Typography variant="h2">Questions</Typography>
-          <Typography variant="body1">
+          <Typography
+          sx={{
+            flexBasis: "100",
+            textAlign: "center",
+            marginTop: "10px",
+            marginBottom: "20px"
+          }}
+          variant="h2">Questions</Typography>
+          <Typography
+          sx={{
+            marginBottom: "16px"
+          }}
+          variant="body1">
             For more information, please visit our{" "}
             <Link variant="primary" to={"/faqs"} component={RouterLink}>
               FAQ page
@@ -239,16 +299,36 @@ const About = () => {
             .
           </Typography>
         </Box>
-        <Box className={classes.team}>
+        <Box
+        sx={{
+          padding: "50px 32px 50px",
+          margin: "32px 0 0 0",
+          borderRadius: "24px",
+          background: "#B6D8FB",
+          display: "flex",
+          flexDirection: "column"
+        }}
+        >
           <img
             alt="Contact Us"
             src={iconSpacerGray}
             className={classes.icon}
             height="40"
           />
-          <Typography variant="h2">Contact Us</Typography>
+          <Typography
+          sx={{
+            flexBasis: "100",
+            textAlign: "center",
+            marginTop: "10px",
+            marginBottom: "20px"
+          }}
+          variant="h2">Contact Us</Typography>
           <Container maxWidth="sm">
-            <Typography variant="body1" align="center">
+            <Typography
+            sx={{
+              marginBottom: "16px"
+            }}
+            variant="body1" align="center">
               Questions about our project?
               <br />
               Updates to the listings?
@@ -268,8 +348,29 @@ const About = () => {
             </Typography>
           </Container>
         </Box>
-        <Box className={classes.partners}>
-          <Typography variant="h2">Our Partners</Typography>
+        <Box
+        sx={{
+          background: "#fff",
+          padding: "48px 32px",
+          borderRadius: "24px",
+          justifyContent: "space-between",
+          alignItems: "center",
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: {xs: "column", md: "row"}
+        }}
+        >
+          <Typography
+          sx={{
+            width: "100%",
+            flexBasis: "100",
+            textAlign: "center",
+            fontWeight: "500",
+            fontSize: "32px",
+            marginTop: "10px",
+            marginBottom: "60px"
+          }}
+          variant="h2">Our Partners</Typography>
           <Link
             component="a"
             href="https://www.foodcyclela.org/"
@@ -280,7 +381,11 @@ const About = () => {
               component="img"
               alt="Food Cycle LA"
               src={foodCycle}
-              className={classes.partnersLogo}
+              sx={{
+                maxWidth: "100%",
+                height: "100%",
+                marginBottom: {xs: "40px", md: "0"}
+              }}
             />
           </Link>
           <Link
@@ -293,7 +398,11 @@ const About = () => {
               component="img"
               alt="Food Forward"
               src={foodForward}
-              className={classes.partnersLogo}
+              sx={{
+                maxWidth: "100%",
+                height: "100%",
+                marginBottom: {xs: "40px", md: "0"}
+              }}
             />
           </Link>
           <Link
@@ -306,7 +415,11 @@ const About = () => {
               component="img"
               alt="Farm People"
               src={farm2people}
-              className={classes.partnersLogo}
+              sx={{
+                maxWidth: "100%",
+                height: "100%",
+                marginBottom: {xs: "40px", md: "0"}
+              }}
             />
           </Link>
           <Link
@@ -319,7 +432,11 @@ const About = () => {
               component="img"
               alt="Food Bank"
               src={foodBank}
-              className={classes.partnersLogo}
+              sx={{
+                maxWidth: "100%",
+                height: "100%",
+                marginBottom: {xs: "40px", md: "0"}
+              }}
             />
           </Link>
         </Box>

0a1161ec703aaa3208fab00d58f22583dd4a8493
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-06-18 08:38:54 -0500

    added mui components and sx prop in register form

--- client/src/components/Account/Register.js
+++ client/src/components/Account/Register.js
@@ -23,31 +23,33 @@ import {
   PASSWORD_VALIDATION_REGEX,
 } from "helpers/Constants";
 import Label from "components/Admin/ui/Label";
+import { palette } from "theme/palette";
 
-const styles = (theme) => ({
-  paper: {
-    marginTop: theme.spacing(1),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  body: {
-    display: "flex",
-    height: "97.8%",
-    flexDirection: "column",
-  },
-  container: {
-    flex: 1,
-  },
-});
+
+ const styles = (theme) => ({
+//   // paper: {
+//   //   marginTop: theme.spacing(1),
+//   //   display: "flex",
+//   //   flexDirection: "column",
+//   //   alignItems: "center",
+//   // },
+//   // avatar: {
+//   //   margin: theme.spacing(1),
+//   //   backgroundColor: theme.palette.secondary.main,
+//   // },
+//   // form: {
+//   //   width: "100%", // Fix IE 11 issue.
+//   //   marginTop: theme.spacing(1),
+//   // },
+//   // body: {
+//   //   display: "flex",
+//   //   height: "97.8%",
+//   //   flexDirection: "column",
+//   // },
+//   // container: {
+//   //   flex: 1,
+//   // },
+ });
 
 // Core component is the Material UI form itself
 const form = (props) => {
@@ -65,11 +67,28 @@ const form = (props) => {
   } = props;
 
   return (
-    <div className={classes.body}>
-      <Container component="main" maxWidth="xs" className={classes.container}>
+    <Container component="main" maxWidth="xs"
+    sx={{
+      display: "flex",
+      height: "97.8%",
+      flexDirection: "column",
+    }}
+    >
         <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
+        <Box
+        sx={{
+          marginTop: "8px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        >
+          <Avatar
+          sx={{
+            margin: "8px",
+            backgroundColor: palette.secondary.main,
+          }}
+          >
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
@@ -78,7 +97,6 @@ const form = (props) => {
           <form className={classes.form} noValidate onSubmit={handleSubmit}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
-                <div>
                   <Label id="firstName" label="First Name *" />
                   <TextField
                     autoComplete="fname"
@@ -94,10 +112,8 @@ const form = (props) => {
                     required
                     value={values.firstName}
                   />
-                </div>
               </Grid>
               <Grid item xs={12} sm={6}>
-                <div>
                   <Label id="lastName" label="Last Name *" />
                   <TextField
                     required
@@ -112,10 +128,8 @@ const form = (props) => {
                     helperText={touched.lastName ? errors.lastName : ""}
                     error={touched.lastName && Boolean(errors.lastName)}
                   />
-                </div>
               </Grid>
               <Grid item xs={12}>
-                <div>
                   <Label id="email" label="Email" />
                   <TextField
                     type="email"
@@ -130,10 +144,8 @@ const form = (props) => {
                     helperText={touched.email ? errors.email : ""}
                     error={touched.email && Boolean(errors.email)}
                   />
-                </div>
               </Grid>
               <Grid item xs={12}>
-                <div>
                   <Label id="password" label="Password *" />
                   <PasswordInput
                     required
@@ -148,10 +160,8 @@ const form = (props) => {
                     helperText={touched.password ? errors.password : ""}
                     error={touched.password && Boolean(errors.password)}
                   />
-                </div>
               </Grid>
               <Grid item xs={12}>
-                <div>
                   <Label id="passwordConfirm" label="Confirm Password *" />
                   <PasswordInput
                     required
@@ -169,7 +179,6 @@ const form = (props) => {
                       touched.passwordConfirm && Boolean(errors.passwordConfirm)
                     }
                   />
-                </div>
               </Grid>
             </Grid>
             <Box sx={{ mt: 2, mb: 2 }}>
@@ -190,9 +199,8 @@ const form = (props) => {
               </Grid>
             </Grid>
           </form>
-        </div>
+        </Box>
       </Container>
-    </div>
   );
 };
 

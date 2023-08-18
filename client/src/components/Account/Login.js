ebc2946dc9898ed496cb74d124377b7cb8b99f52
John Darragh <darragh@entrotech.net>
2023-08-17 20:36:06 -0700

    Fix conditional logic that activates clarity for produciton env only

--- client/src/components/Account/Login.js
+++ client/src/components/Account/Login.js
@@ -3,12 +3,11 @@ import { useLocation, useNavigate, useParams } from "react-router-dom";
 import {
   Avatar,
   Box,
-  Container,
   Link,
   Grid,
   TextField,
   Typography,
-  Button
+  Button,
 } from "@mui/material";
 import { Formik } from "formik";
 import * as Yup from "yup";
@@ -22,8 +21,6 @@ import PasswordInput from "../../components/UI/PasswordInput";
 import Label from "components/Admin/ui/Label";
 import { palette } from "theme/palette";
 import { PageWrapper } from "./PageWrapper";
-
-
 
 const validationSchema = Yup.object().shape({
   email: Yup.string()
@@ -54,192 +51,191 @@ const LoginForm = (props) => {
 
   return (
     <PageWrapper>
-        <Box
+      <Box
         sx={{
           marginTop: "8px",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
-        >
-          {state?.isPasswordReset && (
-            <Typography component="p" className={classes.submit}>
-              Password has been successfully updated
-            </Typography>
-          )}
-          <Avatar
+      >
+        {state?.isPasswordReset && (
+          <Typography component="p" className={classes.submit}>
+            Password has been successfully updated
+          </Typography>
+        )}
+        <Avatar
           sx={{
             margin: "8px",
             backgroundColor: palette.secondary.main,
           }}
-          >
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <Formik
-            validateOnBlur={false}
-            initialValues={{
-              email: email || "",
-              password: "",
-            }}
-            validationSchema={validationSchema}
-            onSubmit={(values, formikBag) => {
-              setTimeout(async () => {
-                try {
-                  const response = await accountService.login(
-                    values.email,
-                    values.password
-                  );
-                  if (response.isSuccess) {
-                    analytics.identify(response.user.id);
-                    onLogin(response.user);
+        >
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Formik
+          validateOnBlur={false}
+          initialValues={{
+            email: email || "",
+            password: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values, formikBag) => {
+            setTimeout(async () => {
+              try {
+                const response = await accountService.login(
+                  values.email,
+                  values.password
+                );
+                if (response.isSuccess) {
+                  analytics.identify(response.user.id);
+                  onLogin(response.user);
+                  setToast({
+                    message: "Login successful.",
+                  });
+                  if (state?.from) {
+                    navigate(state.from);
+                  } else if (
+                    response.user.isAdmin ||
+                    response.user.isCoordinator
+                  ) {
+                    navigate("/verificationAdmin");
+                  } else if (response.user.isSecurityAdmin) {
+                    navigate("/securityadmindashboard");
+                  } else if (response.user.isDataEntry) {
+                    navigate("/verificationdashboard");
+                  } else {
+                    navigate("/");
+                  }
+                } else if (response.code === "AUTH_NOT_CONFIRMED") {
+                  try {
+                    await accountService.resendConfirmationEmail(values.email);
                     setToast({
-                      message: "Login successful.",
-                    });
-                    if (state?.from) {
-                      navigate(state.from);
-                    } else if (
-                      response.user.isAdmin ||
-                      response.user.isCoordinator
-                    ) {
-                      navigate("/verificationAdmin");
-                    } else if (response.user.isSecurityAdmin) {
-                      navigate("/securityadmindashboard");
-                    } else if (response.user.isDataEntry) {
-                      navigate("/verificationdashboard");
-                    } else {
-                      navigate("/");
-                    }
-                  } else if (response.code === "AUTH_NOT_CONFIRMED") {
-                    try {
-                      await accountService.resendConfirmationEmail(
-                        values.email
-                      );
-                      setToast({
-                        message: `Your email has not been confirmed.
+                      message: `Your email has not been confirmed.
                       Please look through your email for a Registration
                       Confirmation link and use it to confirm that you
                       own this email address.`,
-                      });
-                      formikBag.setSubmitting(false);
-                    } catch (err) {
-                      setToast({
-                        message: `An internal error occurred in sending
-                    an email to ${values.email}`,
-                      });
-                      formikBag.setSubmitting(false);
-                    }
-                  } else if (response.code === "AUTH_NO_ACCOUNT") {
-                    console.error("Account not found!!");
-                    setToast({
-                      message: `The email ${values.email} does not correspond to an
-                    existing account. Please verify the email or register as a
-                    new account.`,
                     });
                     formikBag.setSubmitting(false);
-                  } else {
-                    // Presumably response.code === "AUTH_INVALID_PASSWORD"
+                  } catch (err) {
                     setToast({
-                      message: `The password is incorrect, please check it
-                  and try again or use the Forgot Password feature.`,
+                      message: `An internal error occurred in sending
+                    an email to ${values.email}`,
                     });
                     formikBag.setSubmitting(false);
                   }
-                  return true;
-                } catch (err) {
+                } else if (response.code === "AUTH_NO_ACCOUNT") {
+                  console.error("Account not found!!");
                   setToast({
-                    message: "Server error. Please contact support.",
+                    message: `The email ${values.email} does not correspond to an
+                    existing account. Please verify the email or register as a
+                    new account.`,
                   });
-                  console.error(err);
+                  formikBag.setSubmitting(false);
+                } else {
+                  // Presumably response.code === "AUTH_INVALID_PASSWORD"
+                  setToast({
+                    message: `The password is incorrect, please check it
+                  and try again or use the Forgot Password feature.`,
+                  });
                   formikBag.setSubmitting(false);
                 }
-              }, 400);
-            }}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              dirty,
-              isValid,
-            }) => (
-              <form
-              noValidate onSubmit={handleSubmit}>
-                <Grid
+                return true;
+              } catch (err) {
+                setToast({
+                  message: "Server error. Please contact support.",
+                });
+                console.error(err);
+                formikBag.setSubmitting(false);
+              }
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            dirty,
+            isValid,
+          }) => (
+            <form noValidate onSubmit={handleSubmit}>
+              <Grid
                 sx={{
                   width: "100%", // Fix IE 11 issue.
-                  marginTop: "8px"
+                  marginTop: "8px",
                 }}
-                 container spacing={2}>
-                  <Grid item xs={12}>
-                      <Label id="email" label="Email *" />
-                      <TextField
-                        autoComplete="email"
-                        autoFocus
-                        error={touched.email && Boolean(errors.email)}
-                        fullWidth
-                        helperText={touched.email ? errors.email : ""}
-                        id="email"
-                        placeholder="Email"
-                        name="email"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        required
-                        type="email"
-                        value={values.email}
-                      />
-                  </Grid>
-                  <Grid item xs={12}>
-                      <Label id="password" label="Password" />
-                      <PasswordInput
-                        autoComplete="current-password"
-                        error={touched.password && Boolean(errors.password)}
-                        fullWidth
-                        helperText={touched.password ? errors.password : ""}
-                        id="password"
-                        placeholder="Password"
-                        name="password"
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.password}
-                      />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Box>
-                      <Button
-                        type="submit"
-                        disabled={isSubmitting || !(isValid && dirty)}
-                        fullWidth
-                      >
-                        Sign In
-                      </Button>
-                    </Box>
-                  </Grid>
-                  <Grid item xs>
-                    <Link
-                      href={`/forgotpassword/${values.email || ""}`}
-                      variant="body2"
-                    >
-                      Forgot password?
-                    </Link>
-                  </Grid>
-                  <Grid item>
-                    <Link href="/register" variant="body2">
-                      Register
-                    </Link>
-                  </Grid>
+                container
+                spacing={2}
+              >
+                <Grid item xs={12}>
+                  <Label id="email" label="Email *" />
+                  <TextField
+                    autoComplete="email"
+                    autoFocus
+                    error={touched.email && Boolean(errors.email)}
+                    fullWidth
+                    helperText={touched.email ? errors.email : ""}
+                    id="email"
+                    placeholder="Email"
+                    name="email"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    required
+                    type="email"
+                    value={values.email}
+                  />
                 </Grid>
-              </form>
-            )}
-          </Formik>
-        </Box>
-      </PageWrapper>
+                <Grid item xs={12}>
+                  <Label id="password" label="Password" />
+                  <PasswordInput
+                    autoComplete="current-password"
+                    error={touched.password && Boolean(errors.password)}
+                    fullWidth
+                    helperText={touched.password ? errors.password : ""}
+                    id="password"
+                    placeholder="Password"
+                    name="password"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.password}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Box>
+                    <Button
+                      type="submit"
+                      disabled={isSubmitting || !(isValid && dirty)}
+                      fullWidth
+                    >
+                      Sign In
+                    </Button>
+                  </Box>
+                </Grid>
+                <Grid item xs>
+                  <Link
+                    href={`/forgotpassword/${values.email || ""}`}
+                    variant="body2"
+                  >
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/register" variant="body2">
+                    Register
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          )}
+        </Formik>
+      </Box>
+    </PageWrapper>
   );
 };
 

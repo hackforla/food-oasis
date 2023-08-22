eeb2b2170fc96c0bbb01d99d59d3f70072a0e20a
John Darragh <darragh@entrotech.net>
2023-08-21 17:49:01 -0700

    Adjust validation constraints

--- server/app/validation-schema/suggestion-schema.ts
+++ server/app/validation-schema/suggestion-schema.ts
@@ -1,9 +1,11 @@
 import { JSONSchemaType } from "ajv";
 import { Suggestion } from "../../types/suggestion-types";
 
-export const suggestionPostRequestSchema: JSONSchemaType<Omit<Suggestion, "id">> = {
+export const suggestionPostRequestSchema: JSONSchemaType<
+  Omit<Suggestion, "id">
+> = {
   type: "object",
-  required: ["name"],
+  required: ["name", "tenantId"],
   properties: {
     stakeholderId: {
       type: "integer",
@@ -15,82 +17,73 @@ export const suggestionPostRequestSchema: JSONSchemaType<Omit<Suggestion, "id">>
     suggestionStatusId: { type: "integer" },
     name: {
       type: "string",
-      maxLength: 100,
-      pattern: "^[^\s].+[^\s]$"
+      pattern: "^[^s].+[^s]$",
     },
     address1: {
       type: "string",
-      maxLength: 256,
     },
     address2: {
       type: "string",
-      maxLength: 256,
     },
     city: {
       type: "string",
-      maxLength: 20,
     },
     state: {
       type: "string",
-      maxLength: 20,
     },
     zip: {
       type: "string",
-      maxLength: 256,
     },
     phone: {
       type: "string",
-      maxLength: 20,
     },
     email: {
       // once we have set a value to format/pattern property, that property becomes required
-      // since email is opetional, we can make UnionType to accept empty strings
+      // since email is optional, we can make UnionType to accept empty strings
       // setting minLength to 0 or setting nullable: true won't help to leave email empty
       anyOf: [
         {
           type: "string",
-          "format": "email",
-          maxLength: 50,
+          format: "email",
         },
         {
           type: "string",
-          maxLength: 0
-        }
-      ]
+          maxLength: 0,
+        },
+      ],
     },
     notes: { type: "string" }, // also called "other information"
     tipsterName: {
       type: "string",
-      maxLength: 100
     },
     tipsterPhone: {
       type: "string",
-      maxLength: 20,
     },
     tipsterEmail: {
       anyOf: [
         {
           type: "string",
-          "format": "email",
-          maxLength: 50,
+          format: "email",
         },
         {
           type: "string",
-          maxLength: 0
-        }
-      ]
+          maxLength: 0,
+        },
+      ],
     },
     hours: { type: "string" },
     category: { type: "string" },
     tenantId: {
       type: "integer",
-      minimum: 1
-    }
+      minimum: 1,
+    },
   },
-  additionalProperties: false
+  additionalProperties: false,
 };
 
-export const suggestionPutRequestSchema: JSONSchemaType<Omit<Suggestion, "id">> = {
+export const suggestionPutRequestSchema: JSONSchemaType<
+  Omit<Suggestion, "id">
+> = {
   type: "object",
   required: [],
   properties: {
@@ -124,53 +117,47 @@ export const suggestionPutRequestSchema: JSONSchemaType<Omit<Suggestion, "id">> 
     },
     zip: {
       type: "string",
-      maxLength: 256,
     },
     phone: {
       type: "string",
-      maxLength: 20,
     },
     email: {
       anyOf: [
         {
           type: "string",
-          "format": "email",
-          maxLength: 50,
+          format: "email",
         },
         {
           type: "string",
-          maxLength: 0
-        }
-      ]
+          maxLength: 0,
+        },
+      ],
     },
     notes: { type: "string" },
     tipsterName: {
       type: "string",
-      maxLength: 100
     },
     tipsterPhone: {
       type: "string",
-      maxLength: 20,
     },
     tipsterEmail: {
       anyOf: [
         {
           type: "string",
-          "format": "email",
-          maxLength: 50,
+          format: "email",
         },
         {
           type: "string",
-          maxLength: 0
-        }
-      ]
+          maxLength: 0,
+        },
+      ],
     },
     hours: { type: "string" },
     category: { type: "string" },
     tenantId: {
       type: "integer",
-      minimum: 1
-    }
+      minimum: 1,
+    },
   },
-  additionalProperties: false
+  additionalProperties: false,
 };
--- server/app/validation-schema/tag-schema.ts
+++ server/app/validation-schema/tag-schema.ts
@@ -1,5 +1,5 @@
 import { JSONSchemaType } from "ajv";
-import { Tag, StakeholderTag } from '../../types/tag-types';
+import { Tag, StakeholderTag } from "../../types/tag-types";
 
 export const tagPutRequestSchema: JSONSchemaType<Omit<Tag, "id">> = {
   type: "object",
@@ -8,25 +8,24 @@ export const tagPutRequestSchema: JSONSchemaType<Omit<Tag, "id">> = {
     name: {
       type: "string",
       minLength: 1,
-      maxLength: 256,
-    }
+    },
   },
   additionalProperties: false,
 };
 
-export const tagPostRequestSchema: JSONSchemaType<Omit<StakeholderTag, "id">> = {
-  type: "object",
-  required: ["name"],
-  properties: {
-    name: {
-      type: "string",
-      minLength: 1,
-      maxLength: 256
+export const tagPostRequestSchema: JSONSchemaType<Omit<StakeholderTag, "id">> =
+  {
+    type: "object",
+    required: ["name", "tenantId"],
+    properties: {
+      name: {
+        type: "string",
+        minLength: 1,
+      },
+      tenantId: {
+        type: "integer",
+        minimum: 1,
+      },
     },
-    tenantId: {
-      type: "integer",
-      minimum: 1,
-    },
-  },
-  additionalProperties: false
-}
+    additionalProperties: false,
+  };

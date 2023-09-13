756d95f118332abc81e96cae4679aaf42284e0f6
sisi-in-tech <si.sijun@outlook.com>
2023-09-12 19:02:16 -0700

    Update Styling system to Material-UI on StaticPagesHI
    Update styling to Material UI on staticPageHI

--- client/src/components/StaticPagesHI/Components/Page/Page.js
+++ client/src/components/StaticPagesHI/Components/Page/Page.js
@@ -1,55 +1,50 @@
 import React from "react";
-import makeStyles from "@mui/styles/makeStyles";
+import {Container, CardMedia, Box} from "@mui/material";
 import Typography from "@mui/material/Typography";
 
-const useStyles = makeStyles(() => ({
-  outer: {
-    background: "#fff",
-  },
-  main: {
-    padding: "1.5rem 0;",
-    maxWidth: "1200px",
-    margin: "0 auto",
-    "@media only screen and (min-width: 75em)": {
-      padding: "1.5rem 2rem",
-    },
-  },
-  title: {
-    textTransform: "uppercase",
-    fontWeight: 500,
-    textAlign: "center",
-    background: "#FFF",
-    margin: 0,
-    padding: "32px 0",
-  },
-  figure: {
-    margin: 0,
-    padding: 0,
-  },
-}));
 const Page = (props) => {
   const { pageTitle, pageImage, children } = props;
-  const classes = useStyles();
 
   return (
-    <div className={classes.outer}>
-      <div className={classes.main}>
-        <Typography variant="h1" className={classes.title}>
+    <Box sx={{ background: "#fff" }}>
+      <Container 
+        sx={{
+          padding: "1.5rem 0;",
+          maxWidth: "1200px",
+          margin: "0 auto",
+          "@media only screen and (min-width: 75em)": {
+          padding: "1.5rem 2rem",
+          },
+        }}
+      >
+        <Typography variant="h1" 
+          sx={{
+            textTransform: "uppercase",
+            fontWeight: 500,
+            textAlign: "center",
+            background: "#FFF",
+            margin: 0,
+            padding: "32px 0",
+          }}
+        >
           <span>{pageTitle}</span>
         </Typography>
         <br />
         {pageImage && (
-          <figure className={classes.figure}>
-            <img
+            <CardMedia 
+              component="img"
               alt={pageImage.alt}
               src={pageImage.src}
-              style={{ width: "100%", borderRadius: "24px" }}
+              sx={{ width: "100%", 
+                    borderRadius: "24px",
+                    margin: 0, 
+                    padding: 0,
+               }}
             />
-          </figure>
         )}
         {children}
-      </div>
-    </div>
+      </Container>
+    </Box>
   );
 };
 

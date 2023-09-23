e07de529ffcb281b1afd3f4745d40b749469b279
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-09-23 13:56:58 -0500

    removed makestyles and kepts same styles with sx prop and others

--- client/src/components/Layout/WidgetFooter.js
+++ client/src/components/Layout/WidgetFooter.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import makeStyles from "@mui/styles/makeStyles";
 import { isMobile } from "../../helpers";
 import { useSiteContext } from "contexts/siteContext";
+import { Box } from "@mui/material";
 
 const logoPaths = {
   1: require("images/foodoasis.svg"),
@@ -22,27 +22,118 @@ const logoPaths = {
 //   6: require("images/logo-food-oasis-stacked.svg"),
 // };
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    backgroundColor: "#FFF",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    minHeight: "50px",
-    marginBottom: 0,
-    padding: "2px 0.5em",
-    boxShadow: "none",
-    borderBottomColor: theme.palette.primary,
-  },
-  logo: {
-    maxWidth: "175px",
-    maxHeight: "38px",
-    "&:hover": {
-      filter: "brightness(1.2)",
+
+
+function WidgetFooterSection(props) {
+  const {
+    logoPath,
+    alt,
+    className,
+    logoStyle,
+    captionText,
+    name,
+    url,
+    maintainers,
+    type,
+  } = props;
+
+
+  if (type === "maintainer") {
+    if (!maintainers?.length) return null;
+    return (
+      <Box
+      sx={(theme) => ({
+        color: theme.palette.primary.dark,
+        display: "flex",
+        alignItems: "center",
+        lineHeight: "1.2",
+        padding: 0,
+        margin: 0,
+        fontSize: 12,
+        "& a,h6": {
+          whiteSpace: "nowrap",
+          fontSize: 14,
+          textDecoration: "none",
+          color: theme.palette.primary.light,
+        },
+        [theme.breakpoints.up("sm")]: {
+          fontSize: 14,
+        },
+      })
+
+      }
+      >
+        <Box
+        sx={{
+          display: "inline-block",
+          fontWeight: "bold",
+          fontFamily: `Helvetica, Arial, "Lucida Grande", sans- serif`,
+        }}
+        >{captionText}</Box>
+        {maintainers.map((maintainer) => {
+          const logoMaintainerPath = maintainer.path?.default;
+          const imageType = logoMaintainerPath
+            ? logoMaintainerPath.split(".").pop()
+            : "unknown";
+
+          return (
+            <Box style={{ marginLeft: 10 }} key={maintainer.name}>
+              {logoMaintainerPath ? (
+                <a
+                  href={maintainer.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={logoMaintainerPath}
+                    className={className}
+                    style={
+                      imageType === "svg"
+                        ? {
+                            ...logoStyle,
+                            width: "100%",
+                            height: "100%",
+                            margin: 0,
+                            maxWidth: "65px",
+                            maxHeight: "38px",
+                            "&:hover": {
+                              filter: "brightness(1.2)",
     },
-  },
-  footerSectionContainer: {
-    color: theme.palette.primary.dark,
+                            
+                          }
+                        : { ...logoStyle,
+                          maxWidth: "175px",
+                          maxHeight: "38px",
+                          "&:hover": {
+                            filter: "brightness(1.2)",
+    },
+                         }
+                    }
+                    alt={`${maintainer.name} Logo`}
+                  />
+                </a>
+              ) : (
+                <a
+                  href={maintainer.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {maintainer.name}
+                </a>
+              )}
+            </Box>
+          );
+        })}
+      </Box>
+    );
+  }
+
+  const imageType = logoPath ? logoPath.default.split(".").pop() : "unknown";
+
+  return (
+    <Box
+    sx={(theme) => ({
+      color: theme.palette.primary.dark,
     display: "flex",
     alignItems: "center",
     lineHeight: "1.2",
@@ -58,92 +149,25 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up("sm")]: {
       fontSize: 14,
     },
-  },
-  footerCaption: {
-    display: "inline-block",
-    fontWeight: "bold",
-    fontFamily: `Helvetica, Arial, "Lucida Grande", sans- serif`,
-  },
-}));
-
-function WidgetFooterSection(props) {
-  const {
-    logoPath,
-    alt,
-    className,
-    logoStyle,
-    captionText,
-    name,
-    url,
-    maintainers,
-    type,
-  } = props;
-  const classes = useStyles();
-
-  if (type === "maintainer") {
-    if (!maintainers?.length) return null;
-    return (
-      <div className={classes.footerSectionContainer}>
-        <div className={classes.footerCaption}>{captionText}</div>
-        {maintainers.map((maintainer) => {
-          const logoMaintainerPath = maintainer.path?.default;
-          const imageType = logoMaintainerPath
-            ? logoMaintainerPath.split(".").pop()
-            : "unknown";
-
-          return (
-            <div style={{ marginLeft: 10 }} key={maintainer.name}>
-              {logoMaintainerPath ? (
-                <a
-                  href={maintainer.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src={logoMaintainerPath}
-                    className={className || classes.logo}
-                    style={
-                      imageType === "svg"
-                        ? {
-                            ...logoStyle,
-                            width: "100%",
-                            height: "100%",
-                            margin: 0,
-                          }
-                        : { ...logoStyle }
-                    }
-                    alt={`${maintainer.name} Logo`}
-                  />
-                </a>
-              ) : (
-                <a
-                  href={maintainer.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {maintainer.name}
-                </a>
-              )}
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-
-  const imageType = logoPath ? logoPath.default.split(".").pop() : "unknown";
-
-  return (
-    <div className={classes.footerSectionContainer}>
+    })}
+    >
       {logoPath ? (
         <a href={url} target="_blank" rel="noopener noreferrer">
           <img
             src={logoPath ? logoPath.default : logoPath[1].default}
-            className={className || classes.logo}
+            className={className}
             style={
               imageType === "svg"
-                ? { ...logoStyle, width: "100%", height: "100%", margin: 0 }
-                : { ...logoStyle }
+                ? { ...logoStyle, width: "100%", height: "100%", margin: 0,  maxWidth: "175px",
+                maxHeight: "38px",
+                "&:hover": {
+                  filter: "brightness(1.2)",
+                },}
+                : { ...logoStyle, maxWidth: "175px",
+                maxHeight: "38px",
+                "&:hover": {
+                  filter: "brightness(1.2)",
+                }, }
             }
             alt={alt}
           />
@@ -153,7 +177,7 @@ function WidgetFooterSection(props) {
           {name}
         </a>
       )}
-    </div>
+    </Box>
   );
 }
 
@@ -161,29 +185,36 @@ function WidgetFooter() {
   const [mobile, setMobile] = useState(null);
   const { tenantId, tenantDetails } = useSiteContext();
   const { maintainers } = tenantDetails;
-  const classes = useStyles();
 
   useEffect(() => {
     setMobile(isMobile);
   }, []);
 
   return (
-    <div position="sticky" className={classes.container}>
+    <Box position="sticky"
+    sx={(theme) => ({
+      backgroundColor: "#FFF",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    minHeight: "50px",
+    marginBottom: 0,
+    padding: "2px 0.5em",
+    boxShadow: "none",
+    })}>
       <WidgetFooterSection
         name="Food Oasis"
         logoPath={logoPaths[tenantId]}
         url={`${window.location.origin}`}
         alt="Food Oasis Logo"
-        className={classes.logo}
       />
       <WidgetFooterSection
         type="maintainer"
         maintainers={maintainers}
         captionText="A project by"
-        className={classes.logo}
         logoStyle={mobile && { maxWidth: "65px" }}
       />
-    </div>
+    </Box>
   );
 }
 
@@ -210,3 +241,4 @@ WidgetFooterSection.defaultProps = {
 };
 
 export default WidgetFooter;
+

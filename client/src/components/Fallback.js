93b50bd3643984d41d09682e5c1af151708666cb
saumildhankar <saumil.dhankar@yahoo.com>
2023-09-25 02:45:10 -0700

    Update styling with sx prop in fallback
    added component h1 and removed sm md from font size and line height
    
    removed outer box
    
    added width property

--- client/src/components/Fallback.js
+++ client/src/components/Fallback.js
@@ -1,56 +1,46 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import makeStyles from "@mui/styles/makeStyles";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const useStyles = makeStyles(() => ({
-  outer: {
-    background: "#fff",
-  },
-  main: {
-    padding: "1.5rem 0;",
-    maxWidth: "1200px",
-    margin: "0 auto",
-    "@media only screen and (min-width: 75em)": {
-      padding: "1.5rem 2rem",
-    },
-  },
-  figure: {
-    margin: 0,
-    padding: 0,
-  },
-  unauthorized: {
-    padding: "32px",
-    margin: "32px 0",
-    borderRadius: "24px",
-    background: "#f0f0f0",
-    display: "flex",
-    flexDirection: "column",
-    "& $h2": {
-      color: "#336699",
-      flexBasis: "100",
-      textTransform: "uppercase;",
-      textAlign: "center",
-      fontWeight: "500",
-      fontSize: "32px",
-      marginTop: "10px",
-      marginBottom: "20px",
-    },
-  },
-}));
 const Fallback = () => {
-  const classes = useStyles();
   const { state } = useLocation();
 
   return (
-    <div className={classes.outer}>
-      <div className={classes.main}>
-        <figure className={classes.figure}></figure>
-        <div className={classes.unauthorized}>
-          <h2>Unauthorized</h2>
-          <p>{state?.message || "Something went wrong."}</p>
-        </div>
-      </div>
-    </div>
+    <Box
+      sx={{
+        padding: { xs: "1.5rem 0", md: "1.5rem 2rem" },
+        width: "100%",
+        maxWidth: "1200px",
+        margin: "0 auto",
+      }}
+    >
+      <Box
+        sx={{
+          padding: "32px",
+          margin: "32px 0",
+          borderRadius: "24px",
+          background: "#f0f0f0",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography 
+          sx={{
+            color: "#336699",
+            flexBasis: "100",
+            textTransform: "uppercase;",
+            textAlign: "center",
+            fontWeight: "500",
+            fontSize: { xs: "32px" },
+            lineHeight: { xs: "1.5" },
+            marginTop: "10px",
+            marginBottom: "20px",
+          }}
+        variant="h2" component="h1">Unauthorized</Typography>
+        <Typography>{state?.message || "Something went wrong."}</Typography>
+      </Box>
+    </Box>
   );
 };
 

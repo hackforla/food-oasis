451790c3f87e45c856854c97c136421b0ba558b5
qiqicodes <68657634+qiqicodes@users.noreply.github.com>
2023-10-11 22:43:43 -0700

    replaces existing divs with  mui containers

--- client/src/components/StaticPagesSB/Donate.js
+++ client/src/components/StaticPagesSB/Donate.js
@@ -3,8 +3,7 @@ import React from "react";
 import donatebg from "./assets/donate-bg.webp";
 import iconSpacerGray from "./assets/icon-spacer-gray.svg";
 import makeStyles from "@mui/styles/makeStyles";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
+import {Button, CardMedia, Container, Link, Typography} from "@mui/material";
 
 const useStyles = makeStyles(() => ({
   outer: {
@@ -144,76 +143,152 @@ const Donate = () => {
   const classes = useStyles();
   // const { t } = useTranslation("donate");
   return (
-    <div className={classes.outer}>
-      <div className={classes.main}>
-        <figure className={classes.figure}>
-          <img alt="Donate" src={donatebg} style={{ width: "100%" }} />
-        </figure>
-        <Typography variant="h1" className={classes.title}>
+    <Container 
+       sx={{
+        padding: { xs: "1.5rem 0", md: "1.5rem 2rem"},
+         margin: "0 auto",
+         maxWidth: "1200px"
+       }}
+       >
+         <CardMedia component="img" alt="Donate" src={donatebg} style={{ width: "100%" }}></CardMedia>
+        <Typography variant="h1"
+        sx={{
+          textTransform: "uppercase",
+          textAlign: "center",
+          margin: 0,
+          padding: "32px 0"
+        }}
+        >
           Donate
         </Typography>
-        <section className={classes.donate}>
-          <img
-            alt="Why Donate?"
-            src={iconSpacerGray}
-            className={classes.icon}
-            height="40"
-          />
-          <Typography variant="h2">Why Donate?</Typography>
+        <Container
+        sx={{
+          padding: "10px",
+          color: "#4d4d4d",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap"
+        }}
+        >
           <Container maxWidth="sm">
-            <Typography variant="body1">
-              We’ve done so much already as a 100% volunteer-run
-              organization—but we need your help to finish what we set out to
-              do. The admin, development, and marketing costs to expand our
-              directory are extensive. Your tax-deductible donation would help
-              us offset some of those costs.
+             <Typography variant="body1">
+               We’ve done so much already as a 100% volunteer-run
+               organization—but we need your help to finish what we set out to
+               do. The admin, development, and marketing costs to expand our
+               directory are extensive. Your tax-deductible donation would help
+               us offset some of those costs.
             </Typography>
             <Typography variant="body1">
-              Please make donations to our parent organization (Code for
-              America). At the Code for America donation page you will see a
-              text box: “What inspired you to donate today”?{" "}
-              <b>
-                <strong>
-                  Please write Food Oasis and Hack for LA in that box.
-                </strong>
-              </b>{" "}
-              Your donation will get earmarked for Food Oasis.
+               Please make donations to our parent organization (Code for
+               America). At the Code for America donation page you will see a
+               text box: “What inspired you to donate today”?{" "}
+               <b>
+                 <strong>
+                   Please write Food Oasis and Hack for LA in that box.
+                 </strong>
+               </b>{" "}
+               Your donation will get earmarked for Food Oasis.
+              <Link
+                href="//www.codeforamerica.org/donate"
+                target={"_blank"}
+                rel="noopener noreferrer"
+              >
+                Donate
+              </Link>
+              . Our volunteers have done amazing work but we need your help to
+              maintain and expand this important resource. Your tax-deductible
+              donation helps us offset the costs involved with marketing and
+              updating our directory.
             </Typography>
           </Container>
-          <a
-            href="//www.codeforamerica.org/donate"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={classes.btnOutline}
+          <Button
+          sx={{
+            margin: "30px auto",
+            color: "#fff",
+            border: "1px solid #336699",
+            background: "#336699",
+            borderRadius: "6px",
+            padding: "8px 16px",
+            textDecoration: "none",
+            textTransform: "uppercase"
+          }}
+            variant="contained"
+            onClick={() => {}}
           >
             Donate
-          </a>
-        </section>
-        <div className={classes.volunteerSection}>
-          <img
-            alt="Volunteer"
-            src={iconSpacerGray}
-            className={classes.icon}
-            height="40"
-          />
-          <Typography variant="h2">Want to give your time instead?</Typography>
-          <p>(TBD)</p>
-        </div>
-        <section className={classes.cards}>
-          {/* <aside className={classes.signup}>
-            <h3>Get monthly updates on what we&apos;re doing</h3>
-            <a
-              href="//foodoasis.us18.list-manage.com/subscribe?u=40d31892cbbe01312937f7de6&id=42235bb7ce"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={classes.btnWhite}
-            >
-              Signup
-            </a>
-          </aside> */}
-        </section>
-      </div>
-    </div>
+          </Button>
+        </Container>
+      </Container>
+    // <div className={classes.outer}>
+    //   <div className={classes.main}>
+    //     <figure className={classes.figure}>
+    //       <img alt="Donate" src={donatebg} style={{ width: "100%" }} />
+    //     </figure>
+    //     <Typography variant="h1" className={classes.title}>
+    //       Donate
+    //     </Typography>
+    //     <section className={classes.donate}>
+    //       <img
+    //         alt="Why Donate?"
+    //         src={iconSpacerGray}
+    //         className={classes.icon}
+    //         height="40"
+    //       />
+    //       <Typography variant="h2">Why Donate?</Typography>
+    //       <Container maxWidth="sm">
+    //         <Typography variant="body1">
+    //           We’ve done so much already as a 100% volunteer-run
+    //           organization—but we need your help to finish what we set out to
+    //           do. The admin, development, and marketing costs to expand our
+    //           directory are extensive. Your tax-deductible donation would help
+    //           us offset some of those costs.
+    //         </Typography>
+    //         <Typography variant="body1">
+    //           Please make donations to our parent organization (Code for
+    //           America). At the Code for America donation page you will see a
+    //           text box: “What inspired you to donate today”?{" "}
+    //           <b>
+    //             <strong>
+    //               Please write Food Oasis and Hack for LA in that box.
+    //             </strong>
+    //           </b>{" "}
+    //           Your donation will get earmarked for Food Oasis.
+    //         </Typography>
+    //       </Container>
+    //       <a
+    //         href="//www.codeforamerica.org/donate"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //         className={classes.btnOutline}
+    //       >
+    //         Donate
+    //       </a>
+    //     </section>
+    //     <div className={classes.volunteerSection}>
+    //       <img
+    //         alt="Volunteer"
+    //         src={iconSpacerGray}
+    //         className={classes.icon}
+    //         height="40"
+    //       />
+    //       <Typography variant="h2">Want to give your time instead?</Typography>
+    //       <p>(TBD)</p>
+    //     </div>
+    //     <section className={classes.cards}>
+    //       {/* <aside className={classes.signup}>
+    //         <h3>Get monthly updates on what we&apos;re doing</h3>
+    //         <a
+    //           href="//foodoasis.us18.list-manage.com/subscribe?u=40d31892cbbe01312937f7de6&id=42235bb7ce"
+    //           target="_blank"
+    //           rel="noopener noreferrer"
+    //           className={classes.btnWhite}
+    //         >
+    //           Signup
+    //         </a>
+    //       </aside> */}
+    //     </section>
+    //   </div>
+    // </div>
   );
 };
 

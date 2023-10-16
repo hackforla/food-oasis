e30bff56e14fd90a5e3bee3b85e436b3317d0c65
Virginia Wu <virginiawu11@gmail.com>
2023-10-15 20:52:26 -0400

    1839 remove makestyles progressBackdrop

--- client/src/components/Admin/ImportOrganizations/ProgressBackdrop.js
+++ client/src/components/Admin/ImportOrganizations/ProgressBackdrop.js
@@ -2,35 +2,8 @@ import React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Backdrop from "@mui/material/Backdrop";
 import Typography from "@mui/material/Typography";
-import makeStyles from "@mui/styles/makeStyles";
 
 // Cancel button removes ProgressBackdrop, but doesn't cancel backend read stream, so commented out for now.
-
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: "#fff",
-  },
-  buttonProgress: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginTop: -12,
-    marginLeft: -12,
-  },
-  messageOnLoad: {
-    position: "absolute",
-    top: "55%",
-    marginTop: -12,
-    marginLeft: -12,
-  },
-  buttonCancel: {
-    position: "absolute",
-    top: "60%",
-    marginTop: -12,
-    marginLeft: -12,
-  },
-}));
 
 function ProgressBackdrop(props) {
   const {
@@ -38,18 +11,24 @@ function ProgressBackdrop(props) {
     messageOnLoad,
     // handleCancelUpload
   } = props;
-  const classes = useStyles();
 
   return (
-    <Backdrop open={loading} className={classes.backdrop}>
-      <CircularProgress className={classes.buttonProgress} />
-      {messageOnLoad && (
-        <Typography className={classes.messageOnLoad}>
-          {messageOnLoad}
-        </Typography>
-      )}
+    <Backdrop
+      open={loading}
+      sx={{
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+        color: "#fff",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress sx={{ mt: 5 }} />
+      {messageOnLoad && <Typography sx={{ mt: 1 }}>{messageOnLoad}</Typography>}
+
       {/* <Button
-        onClick={handleCancelUpload}
+        sx={{ mt: 2 }}
         className={classes.buttonCancel}
         variant="contained"
       >

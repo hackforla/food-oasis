a0dbdbfb3bcef73c6a9bcc3b0d4890d78e9a65c4
Qiqi Zheng <68657634+qiqicodes@users.noreply.github.com>
2023-10-16 19:57:15 -0700

    Update Styling System to Use sx Prop in Material-UI Components in StaticPagesSB/Donate.js  (#1879)
    * replaces existing divs with  mui containers
    
    * updates the SB static donate page with material components
    
    * removes makeStyles

--- client/src/components/StaticPagesSB/Donate.js
+++ client/src/components/StaticPagesSB/Donate.js
@@ -1,162 +1,75 @@
 import React from "react";
-
 import donatebg from "./assets/donate-bg.webp";
 import iconSpacerGray from "./assets/icon-spacer-gray.svg";
-import makeStyles from "@mui/styles/makeStyles";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
+import { Box, CardMedia, Container, Link, Typography } from "@mui/material";
 
-const useStyles = makeStyles(() => ({
-  outer: {
-    background: "#fff",
-  },
-  main: {
-    padding: "1.5rem 0;",
-    maxWidth: "1200px",
-    margin: "0 auto",
-    "@media only screen and (min-width: 75em)": {
-      padding: "1.5rem 2rem",
-    },
-  },
-  title: {
-    textTransform: "uppercase",
-    fontWeight: 500,
-    textAlign: "center",
-    background: "#FFF",
-    margin: 0,
-    padding: "32px 0",
-  },
-  btnOrange: {
-    color: "#fff",
-    fontSize: "18px",
-    background: "#e57109",
-    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-    borderRadius: "6px",
-    padding: "8px 16px",
-    textDecoration: "none",
-    textTransform: "uppercase",
-  },
-  btnOutline: {
-    color: "#fff",
-    border: "1px solid #336699",
-    background: "#336699",
-    borderRadius: "6px",
-    padding: "8px 16px",
-    textDecoration: "none",
-    textTransform: "uppercase",
-  },
-  btnWhite: {
-    color: "#336699",
-    background: "#ffffff",
-    borderRadius: "6px",
-    padding: "8px 16px",
-    textDecoration: "none",
-    textTransform: "uppercase",
-  },
-  figure: {
-    margin: 0,
-    padding: 0,
-  },
-  icon: {
-    margin: "auto",
-  },
-  donate: {
-    padding: "32px",
-    margin: "32px 0 0 0",
-    borderRadius: "24px",
-    background: "#f0f0f0",
-    display: "flex",
-    flexWrap: "wrap",
-    flexDirection: "column",
-    "& $h2": {
-      flexBasis: "100%",
-      textAlign: "center",
-      fontWeight: "500",
-      fontSize: "32px",
-      marginTop: "10px",
-      marginBottom: "20px",
-    },
-    "& $btnOutline": {
-      margin: "20px auto 0 auto",
-    },
-    "& $p": {
-      marginBottom: "16px",
-    },
-  },
-  volunteerSection: {
-    padding: "32px",
-    margin: "32px 0 0 0",
-    borderRadius: "24px",
-    background: "#B6D8FB",
-    display: "flex",
-    flexWrap: "wrap",
-    "& $h2": {
-      flexBasis: "100%",
-      textAlign: "center",
-      fontWeight: "500",
-      fontSize: "32px",
-      marginTop: "10px",
-      marginBottom: "20px",
-    },
-    "& $btnWhite": {
-      margin: "20px auto 0 auto",
-    },
-  },
-  cards: {
-    display: "flex",
-    flexWrap: "wrap",
-    margin: "0 0 32px 0",
-    justifyContent: "center",
-    "& $aside": {
-      textAlign: "center",
-      borderRadius: "24px",
-      padding: "1.25rem",
-      maxWidth: "550px",
-      margin: "1rem 0",
-      "& $btnWhite": {
-        display: "inline-block",
-        marginBottom: "20px",
-      },
-      "& $h3": {
-        fontSize: "40px",
-        fontWeight: "500",
-        color: "#ffffff",
-        margin: "20px 0",
-      },
-      "@media only screen and (min-width: 64em)": {
-        marginLeft: "1rem",
-        marginRight: "1rem",
-      },
-    },
-    "@media only screen and (min-width: 64em)": {
-      flexWrap: "nowrap",
-      padding: "32px",
-    },
-  },
-  signup: {
-    background: "rgba(229, 113, 9, .7)",
-  },
-  volunteer: {
-    background: "#e57109",
-  },
-}));
 const Donate = () => {
-  const classes = useStyles();
   // const { t } = useTranslation("donate");
   return (
-    <div className={classes.outer}>
-      <div className={classes.main}>
-        <figure className={classes.figure}>
-          <img alt="Donate" src={donatebg} style={{ width: "100%" }} />
-        </figure>
-        <Typography variant="h1" className={classes.title}>
+    <Container
+      sx={{
+        background: "#fff",
+      }}
+    >
+      <Box
+        sx={{
+          "padding": "1.5rem 0",
+          "maxWidth": "1200px",
+          "margin": "0 auto",
+          "@media only screen and (min-width: 75em)": {
+            padding: "1.5rem 2rem",
+          },
+        }}
+      >
+        <CardMedia
+          component="img"
+          alt="Donate"
+          src={donatebg}
+          style={{ width: "100%" }}
+        ></CardMedia>
+        <Typography
+          variant="h1"
+          sx={{
+            textTransform: "uppercase",
+            fontWeight: 500,
+            textAlign: "center",
+            background: "#FFF",
+            margin: 0,
+            padding: "32px 0",
+          }}
+        >
           Donate
         </Typography>
-        <section className={classes.donate}>
-          <img
+        <Box
+          component="section"
+          sx={{
+            "padding": "32px",
+            "margin": "32px 0 0 0",
+            "borderRadius": "24px",
+            "background": "#f0f0f0",
+            "display": "flex",
+            "flexWrap": "wrap",
+            "flexDirection": "column",
+            "& h2": {
+              flexBasis: "100%",
+              textAlign: "center",
+              fontWeight: "500",
+              fontSize: "32px",
+              mt: "10px",
+              mb: "20px",
+            },
+            "& p": {
+              marginBottom: "16px",
+            },
+          }}
+        >
+          <Box
+            component="img"
             alt="Why Donate?"
             src={iconSpacerGray}
-            className={classes.icon}
+            sx={{
+              margin: "auto",
+            }}
             height="40"
           />
           <Typography variant="h2">Why Donate?</Typography>
@@ -180,40 +93,117 @@ const Donate = () => {
               Your donation will get earmarked for Food Oasis.
             </Typography>
           </Container>
-          <a
+          <Link
+            variant="contained"
             href="//www.codeforamerica.org/donate"
             target="_blank"
             rel="noopener noreferrer"
-            className={classes.btnOutline}
+            sx={{
+              color: "#fff",
+              border: "1px solid #336699",
+              background: "#336699",
+              borderRadius: "6px",
+              padding: "8px 16px",
+              textDecoration: "none",
+              textTransform: "uppercase",
+              margin: "20px auto 0 auto",
+            }}
           >
             Donate
-          </a>
-        </section>
-        <div className={classes.volunteerSection}>
-          <img
+          </Link>
+        </Box>
+        <Box
+          sx={{
+            "padding": "32px",
+            "margin": "32px 0 0 0",
+            "borderRadius": "24px",
+            "background": "#B6D8FB",
+            "display": "flex",
+            "flexWrap": "wrap",
+            "& h2": {
+              flexBasis: "100%",
+              textAlign: "center",
+              fontWeight: "500",
+              fontSize: "32px",
+              mt: "10px",
+              mb: "20px",
+            },
+          }}
+        >
+          <Box
+            component="img"
             alt="Volunteer"
             src={iconSpacerGray}
-            className={classes.icon}
+            sx={{
+              margin: "auto",
+            }}
             height="40"
           />
           <Typography variant="h2">Want to give your time instead?</Typography>
           <p>(TBD)</p>
-        </div>
-        <section className={classes.cards}>
-          {/* <aside className={classes.signup}>
-            <h3>Get monthly updates on what we&apos;re doing</h3>
-            <a
+        </Box>
+        {/* <Box
+          component="section"
+          sx={{
+            "display": "flex",
+            "flexWrap": "wrap",
+            "borderRadius": "24px",
+            "margin": "0 0 32px 0",
+            "justifyContent": "center",
+            "background": "rgba(229, 113, 9, .7)",
+            "@media only screen and (min-width: 64em)": {
+              flexWrap: "nowrap",
+              padding: "32px",
+            },
+          }}
+        >
+          <Box
+            component="aside"
+            sx={{
+              "textAlign": "center",
+              "borderRadius": "24px",
+              "padding": "1.25rem",
+              "maxWidth": "550px",
+              "margin": "1rem 0",
+              "& h3": {
+                fontSize: "40px",
+                fontWeight: "500",
+                color: "#ffffff",
+                margin: "20px 0",
+              },
+              "@media only screen and (min-width: 64em)": {
+                marginLeft: "1rem",
+                marginRight: "1rem",
+              },
+            }}
+          >
+            <Typography variant="h3">
+              Get monthly updates on what we're doing
+            </Typography>
+            <Link
+              variant="contained"
               href="//foodoasis.us18.list-manage.com/subscribe?u=40d31892cbbe01312937f7de6&id=42235bb7ce"
               target="_blank"
               rel="noopener noreferrer"
-              className={classes.btnWhite}
+              sx={{
+                display: "inline-block",
+                marginBottom: "20px",
+                color: "#336699",
+                border: "1px solid #ffffff",
+                background: "#ffffff",
+                borderRadius: "6px",
+                padding: "8px 16px",
+                textDecoration: "none",
+                textTransform: "uppercase",
+                margin: "20px auto 0 auto",
+              }}
             >
               Signup
-            </a>
-          </aside> */}
-        </section>
-      </div>
-    </div>
+            </Link>
+          </Box>
+        </Box> */}
+      </Box>
+    </Container>
   );
 };
 

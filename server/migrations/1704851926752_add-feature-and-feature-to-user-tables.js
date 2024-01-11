9d1fe8227624bad74cfc33a3678dc423370df1e9
June Zhao <bz.bijun@gmail.com>
2024-01-11 11:05:31 -0800

    create tables for featureflag and feature-to-user

---
+++ server/migrations/1704851926752_add-feature-and-feature-to-user-tables.js
@@ -0,0 +1,37 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.sql(`
+  CREATE TABLE feature_flag (
+    id integer primary key,
+    name character varying NOT NULL
+    );
+  `);
+  pgm.sql(`
+  CREATE TABLE feature_to_login (
+    id integer primary key,
+    feature_id integer NOT NULL,
+    login_id integer NOT NULL
+    );
+  `);
+  pgm.sql(`
+  ALTER TABLE public.feature_to_login 
+  ADD CONSTRAINT 
+    fk_feature_flag_feature_to_login
+    FOREIGN KEY (feature_id) REFERENCES feature_flag(id),
+    ADD CONSTRAINT 
+    fk_login_feature_to_login
+    FOREIGN KEY (login_id) REFERENCES login(id)
+  `);
+};
+
+exports.down = (pgm) => {
+  pgm.sql(`
+  DROP TABLE feature_flag;
+  `);
+  pgm.sql(`
+  DROP TABLE feature_to_login;
+  `);
+};

5c932479e51eb6965208abe524ff0cf5db44fdd6
Qiqi Zheng <qiqizheng0127@gmail.com>
2024-01-24 23:18:00 -0800

    migrates from v2 to v3

--- server/app/services/aws-service.ts
+++ server/app/services/aws-service.ts
@@ -1,71 +1,90 @@
-import AWS from "aws-sdk";
+import {
+  Location,
+  SearchPlaceIndexForTextCommandInput,
+  SearchPlaceIndexForTextCommandOutput,
+} from "@aws-sdk/client-location";
+import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 
 const generateLocation = async () => {
   if (!process.env.IDENTITY_POOL_ID) return;
-  AWS.config.update({ region: "us-west-2" });
+
+  // Create a Cognito Identity Client
+  const cognitoClient = new CognitoIdentityClient({ region: "us-west-2" });
   const poolId: string = process.env.IDENTITY_POOL_ID;
-  const credentials: AWS.CognitoIdentityCredentials =
-    new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: poolId,
-    });
-  const location = new AWS.Location({
-    credentials,
-    region: AWS.config.region || "us-west-2",
+
+  // Create Cognito Identity Credentials
+  const credentials = fromCognitoIdentityPool({
+    client: cognitoClient,
+    identityPoolId: poolId,
   });
+
+  // Create a Location client with the credentials and region
+  const location = new Location({
+    credentials,
+    region: cognitoClient.config.region || "us-west-2",
+  });
+
   return location;
 };
 
 const autocomplete = async (address: string, tenantId: number) => {
   if (!process.env.PLACE_INDEX_NAME) return;
   const location = await generateLocation();
-  const params: AWS.Location.SearchPlaceIndexForTextRequest = {
+  const params: SearchPlaceIndexForTextCommandInput = {
     IndexName: process.env.PLACE_INDEX_NAME,
     Text: address,
     MaxResults: 10,
     FilterCountries: ["USA"],
     Language: "en",
   };
+
   if (!location) return;
-  const data = await location
-    .searchPlaceIndexForText(params)
-    .promise()
-    .then((results) => results);
+
+  // Use the Location client to searchPlaceIndexForText
+  const data: SearchPlaceIndexForTextCommandOutput =
+    await location.searchPlaceIndexForText(params);
+
   const tenantRegions: { [key: number]: string } = {
     1: "California",
     3: "Hawaii",
     5: "Texas",
     6: "California",
   };
-  const filtered = data.Results.filter(
-    (result) => result.Place.Region === tenantRegions[tenantId]
+
+  const filtered = data.Results?.filter(
+    (result) => result.Place?.Region === tenantRegions[tenantId]
   );
-  filtered.forEach((result) => {
-    const point: number | undefined = result.Place.Geometry.Point?.shift();
+
+  filtered?.forEach((result) => {
+    const point = result.Place?.Geometry?.Point?.shift();
     if (!point) return;
-    result.Place.Geometry.Point?.push(point);
+    result.Place?.Geometry?.Point?.push(point);
   });
+
   return filtered;
 };
 
 const getCoords = async (address: string) => {
   if (!process.env.PLACE_INDEX_NAME) return;
   const location = await generateLocation();
-  const params: AWS.Location.SearchPlaceIndexForTextRequest = {
+  const params: SearchPlaceIndexForTextCommandInput = {
     IndexName: process.env.PLACE_INDEX_NAME,
     Text: address,
     MaxResults: 1,
     FilterCountries: ["USA"],
     Language: "en",
   };
+
   if (!location) return;
-  const data = await location
-    .searchPlaceIndexForText(params)
-    .promise()
-    .then((results) => results);
-  const point: number | undefined =
-    data.Results[0].Place.Geometry.Point?.shift();
+
+  const data: SearchPlaceIndexForTextCommandOutput =
+    await location.searchPlaceIndexForText(params);
+
+  const point = data.Results?.[0]?.Place?.Geometry?.Point?.shift();
   if (!point) return;
-  data.Results[0].Place.Geometry.Point?.push(point);
+  data.Results?.[0]?.Place?.Geometry?.Point?.push(point);
+
   return data;
 };
 

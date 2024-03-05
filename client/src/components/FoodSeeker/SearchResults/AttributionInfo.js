61b3482978c6b8863b4d8b95f6210b775ec680a5
saumildhankar <saumil.dhankar@yahoo.com>
2024-03-04 17:42:23 -0800

    moved attribution icon to top left
    changed 56px to 60 px
    
    padding stack and ternary
    
    edited spacing
    
    edited spacing

--- client/src/components/FoodSeeker/SearchResults/AttributionInfo.js
+++ client/src/components/FoodSeeker/SearchResults/AttributionInfo.js
@@ -36,11 +36,37 @@ const AttributionInfo = () => {
       ) : (
         <>
           <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <button
+              style={{
+                backgroundColor: "white",
+                borderBottomLeftRadius: "12px",
+                borderTopLeftRadius: "12px",
+                width: "24px",
+                height: "24px",
+                border: "none",
+              }}
+              onClick={handleClick}
+            >
+              <svg
+                style={{
+                  backgroundColor: "white",
+                  borderBottomRightRadius: "12px",
+                  borderTopRightRadius: "12px",
+                }}
+                width="24"
+                height="24"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="evenodd"
+              >
+                <path d="M4 10a6 6 0 1 0 12 0 6 6 0 1 0-12 0m5-3a1 1 0 1 0 2 0 1 1 0 1 0-2 0m0 3a1 1 0 1 1 2 0v3a1 1 0 1 1-2 0" />
+              </svg>
+            </button>
             <div
               style={{
                 backgroundColor: "white",
-                borderTopLeftRadius: "12px",
-                borderBottomLeftRadius: "12px",
+                borderTopRightRadius: "12px",
+                borderBottomRightRadius: "12px",
               }}
             >
               <a
@@ -83,6 +109,7 @@ const AttributionInfo = () => {
                   fontSize: "12px",
                   marginLeft: "3px",
                   fontWeight: "700",
+                  paddingRight: "5px"
                 }}
                 href="https://apps.mapbox.com/feedback/?owner=mapbox&amp;id=streets-v11&amp;access_token=pk.eyJ1IjoibHVjYXNob21lciIsImEiOiJjazFqcnRjcm0wNmZ1M2JwZXg2eDFzMXd3In0.yYpkKLrFCxF-qyBfZH1a8w#/-118.2439/34.0355/11"
                 target="_blank"
@@ -92,32 +119,6 @@ const AttributionInfo = () => {
                 Improve this map
               </a>
             </div>
-            <button
-              style={{
-                backgroundColor: "white",
-                borderBottomRightRadius: "12px",
-                borderTopRightRadius: "12px",
-                width: "24px",
-                height: "24px",
-                border: "none",
-              }}
-              onClick={handleClick}
-            >
-              <svg
-                style={{
-                  backgroundColor: "white",
-                  borderBottomRightRadius: "12px",
-                  borderTopRightRadius: "12px",
-                }}
-                width="24"
-                height="24"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-                fillRule="evenodd"
-              >
-                <path d="M4 10a6 6 0 1 0 12 0 6 6 0 1 0-12 0m5-3a1 1 0 1 0 2 0 1 1 0 1 0-2 0m0 3a1 1 0 1 1 2 0v3a1 1 0 1 1-2 0" />
-              </svg>
-            </button>
           </div>
         </>
       )}

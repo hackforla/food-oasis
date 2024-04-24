cca257c3b5286a1a279a8fe5256dce75d96445ec
Krista Strucke <kstrucke@gmail.com>
2024-04-23 19:10:03 -0700

    fixed typescript bugs and refactored code to dry it out

--- server/app/validation-schema/suggestion-schema.ts
+++ server/app/validation-schema/suggestion-schema.ts
@@ -1,73 +1,68 @@
 import { JSONSchemaType } from "ajv";
 import { Suggestion } from "../../types/suggestion-types";
 
-export const suggestionPostRequestSchema: JSONSchemaType<
-  Omit<Suggestion, "id">
-> = {
-  type: "object",
-  required: ["name", "tenantId"],
-  properties: {
-    stakeholderId: {
-      type: "integer",
-      nullable: true,
-      anyOf: [{ minimum: 1 }, { type: ["integer", "null"] }],
+export type SuggestionPostFields = Omit<
+  Suggestion,
+  "id" | "suggestionStatusId" | "stakeholderId" | "adminNotes"
+>;
+
+export const suggestionPostRequestSchema: JSONSchemaType<SuggestionPostFields> =
+  {
+    type: "object",
+    required: ["name", "tenantId"],
+    properties: {
+      name: {
+        type: "string",
+        minLength: 1,
+      },
+      address1: {
+        type: "string",
+      },
+      address2: {
+        type: "string",
+      },
+      city: {
+        type: "string",
+      },
+      state: {
+        type: "string",
+      },
+      zip: {
+        type: "string",
+      },
+      phone: {
+        type: "string",
+      },
+      email: {
+        // once we have set a value to format/pattern property, that property becomes required
+        // since email is optional, we can make UnionType to accept empty strings
+        // setting minLength to 0 or setting nullable: true won't help to leave email empty
+        type: "string",
+        anyOf: [
+          { format: "email" },
+          { maxLength: 0 }, // for an empty string
+        ],
+      },
+      notes: { type: "string" }, // also called "other information"
+      tipsterName: {
+        type: "string",
+      },
+      tipsterPhone: {
+        type: "string",
+      },
+      tipsterEmail: {
+        type: "string",
+        anyOf: [{ format: "email" }, { maxLength: 0 }],
+      },
+      hours: { type: "string" },
+      category: { type: "string" },
+      tenantId: {
+        type: "integer",
+        minimum: 1,
+      },
     },
-    adminNotes: {
-      type: "string",
-    },
-    suggestionStatusId: { type: ["integer", "null"], nullable: true },
-    name: {
-      type: ["string", "null"],
-      minLength: 1,
-    },
-    address1: {
-      type: "string",
-    },
-    address2: {
-      type: "string",
-    },
-    city: {
-      type: "string",
-    },
-    state: {
-      type: "string",
-    },
-    zip: {
-      type: "string",
-    },
-    phone: {
-      type: "string",
-    },
-    email: {
-      // once we have set a value to format/pattern property, that property becomes required
-      // since email is optional, we can make UnionType to accept empty strings
-      // setting minLength to 0 or setting nullable: true won't help to leave email empty
-      type: "string",
-      anyOf: [
-        { format: "email" },
-        { maxLength: 0 }, // for an empty string
-      ],
-    },
-    notes: { type: "string" }, // also called "other information"
-    tipsterName: {
-      type: "string",
-    },
-    tipsterPhone: {
-      type: "string",
-    },
-    tipsterEmail: {
-      type: "string",
-      anyOf: [{ format: "email" }, { maxLength: 0 }],
-    },
-    hours: { type: "string" },
-    category: { type: "string" },
-    tenantId: {
-      type: "integer",
-      minimum: 1,
-    },
-  },
-  additionalProperties: true,
-};
+    additionalProperties: true,
+  };
 
 export const suggestionPutRequestSchema: JSONSchemaType<Suggestion> = {
   type: "object",

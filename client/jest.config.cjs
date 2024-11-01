9a48eb6e91ffe0fc2a3abed9abb1627ad6946dc6
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2024-10-31 19:08:37 -0700

    Setup client tests (#2268)

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -5,11 +5,22 @@ const { compilerOptions } = require("./tsconfig");
 module.exports = {
   roots: ["<rootDir>"],
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: "./jest.environment.cjs",
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths ?? {}),
   transform: {
     ".+\\.(css|less|sass|scss|png|jpg|gif|ttf|woff|woff2|svg)$":
       "jest-transform-stub",
+    "^.+\\.[t|j]sx?$": "babel-jest",
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!' + 
+        [
+          'debounce-fn',
+          'mimic-fn',,
+        ].join('|') +
+    ')',
+],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"]
 };
---
+++ client/setupTests.ts
@@ -0,0 +1,4 @@
+process.env.VITE_TENANT_ID="1"
+process.env.VITE_MAPBOX_ACCESS_TOKEN="vite-access-token"
+
+export {}

b77e76cc3f75ee770a033ca36c4264a6437935e4
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2024-11-14 14:05:43 -0800

    Update social links (#2279)

--- client/src/components/Admin/OrganizationEdit/ContactDetails.jsx
+++ client/src/components/Admin/OrganizationEdit/ContactDetails.jsx
@@ -1,57 +1,9 @@
 import { Grid } from "@mui/material";
 import { TabPanel } from "components/Admin/ui/TabPanel";
-import { InputAdornment, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
 import Label from "../ui/Label";
 
-const CustomTextFieldComponent = ({
-  id,
-  name,
-  placeholder,
-  value,
-  onChange,
-  onBlur,
-  touched,
-  errors,
-  startAdornment,
-}) => (
-  <TextField
-    id={id}
-    sx={{
-      "& .MuiOutlinedInput-root": {
-        borderRadius: "4px",
-        backgroundColor: "white",
-        paddingLeft: "0",
-        "& fieldset": {
-          borderColor: "grey",
-        },
-        "& input::placeholder": {
-          color: "#B0BEC5",
-          opacity: 1,
-        },
-      },
-      "& .MuiInputAdornment-root": {
-        backgroundColor: "#F4F6F8",
-        padding: "22px 8px 22px 8px",
-        borderRight: "1px solid grey",
-        borderRadius: "4px 0 0 4px",
-        color: "#B0BEC5",
-        margin: "-20px 0",
-      },
-    }}
-    name={name}
-    placeholder={placeholder}
-    value={value}
-    onChange={onChange}
-    onBlur={onBlur}
-    helperText={touched ? errors : ""}
-    error={Boolean(touched && errors)}
-    InputProps={{
-      startAdornment: startAdornment && (
-        <InputAdornment position="start">{startAdornment}</InputAdornment>
-      ),
-    }}
-  />
-);
+
 
 export default function ContactDetails({
   tabPage,
@@ -68,13 +20,13 @@ export default function ContactDetails({
           <div>
             <Label
               id="website"
-              label="Web Site"
+              label="Website"
               tooltipTitle="The organization's web address"
             />
             <TextField
               id="website"
               name="website"
-              placeholder="Web Site"
+              placeholder="Website"
               value={values.website}
               onChange={handleChange}
               onBlur={handleBlur}
@@ -88,20 +40,18 @@ export default function ContactDetails({
             <Label
               id="instagram"
               label="Instagram"
-              tooltipTitle="Enter your Instagram username"
-              href="https://instagram.com/"
-              handle={values.instagram}
+              tooltipTitle="Enter Instagram url"
+              href={values.instagram}
             />
-            <CustomTextFieldComponent
+            <TextField
               id="instagram"
               name="instagram"
-              placeholder="Instagram username"
+              placeholder="https://instagram.com/..."
               value={values.instagram}
               onChange={handleChange}
               onBlur={handleBlur}
-              touched={touched.instagram}
-              errors={errors.instagram}
-              startAdornment="https://instagram.com/"
+              helperText={touched.instagram ? errors.instagram : ""}
+              error={touched.instagram && Boolean(errors.instagram)}
             />
           </div>
         </Grid>
@@ -111,19 +61,17 @@ export default function ContactDetails({
               id="facebook"
               label="Facebook"
               tooltipTitle="Enter your Facebook username"
-              href="https://facebook.com/"
-              handle={values.facebook}
+              href={values.facebook}
             />
-            <CustomTextFieldComponent
+            <TextField
               id="facebook"
               name="facebook"
-              placeholder="Facebook username"
+              placeholder="https://facebook.com/..."
               value={values.facebook}
               onChange={handleChange}
               onBlur={handleBlur}
-              touched={touched.facebook}
-              errors={errors.facebook}
-              startAdornment="https://facebook.com/"
+              helperText={touched.facebook ? errors.facebook : ""}
+              error={touched.facebook && Boolean(errors.facebook)}
             />
           </div>
         </Grid>
@@ -132,20 +80,18 @@ export default function ContactDetails({
             <Label
               id="twitter-label"
               label="Twitter"
-              tooltipTitle="Enter your Twitter username"
-              href="https://twitter.com/"
-              handle={values.twitter}
+              tooltipTitle="Enter Twitter url"
+              href={values.twitter}
             />
-            <CustomTextFieldComponent
+            <TextField
               id="twitter"
               name="twitter"
-              placeholder="Twitter username"
+              placeholder="https://x.com/..."
               value={values.twitter}
               onChange={handleChange}
               onBlur={handleBlur}
-              touched={touched.twitter}
-              errors={errors.twitter}
-              startAdornment="https://twitter.com/"
+              helperText={touched.twitter ? errors.twitter : ""}
+              error={touched.twitter && Boolean(errors.twitter)}
             />
           </div>
         </Grid>
@@ -154,30 +100,33 @@ export default function ContactDetails({
             <Label
               id="pinterest"
               label="Pinterest"
-              href="https://pinterest.com/"
-              handle={values.pinterest}
-              tooltipTitle="Enter your Pinterest username"
+              href={values.pinterest}
+              tooltipTitle="Enter Pinterest url"
             />
-            <CustomTextFieldComponent
+            <TextField
               id="pinterest"
               name="pinterest"
-              placeholder="Pinterest username"
+              placeholder="https://pinterest.com/..."
               value={values.pinterest}
               onChange={handleChange}
               onBlur={handleBlur}
-              touched={touched.pinterest}
-              errors={errors.pinterest}
-              startAdornment="https://pinterest.com/"
+              helperText={touched.pinterest ? errors.pinterest : ""}
+              error={touched.pinterest && Boolean(errors.pinterest)}
             />
           </div>
         </Grid>
         <Grid item sm={6} xs={12}>
           <div>
-            <Label id="linkedin" label="LinkedIn" />
+            <Label
+              id="linkedin"
+              label="LinkedIn"
+              href={values.linkedin}
+              tooltipTitle="Enter Linkedin url"
+            />
             <TextField
               id="linkedin"
               name="linkedin"
-              placeholder="LinkedIn"
+              placeholder="https://linkedin.com/..."
               value={values.linkedin}
               onChange={handleChange}
               onBlur={handleBlur}

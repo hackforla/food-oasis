402afb30a4fc21596e5344796121932f022125e0
June Zhao <60997220+junjun107@users.noreply.github.com>
2025-01-24 22:08:10 -0800

    Fix registration and pw rest redirect (#2333)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
-  base: "",
+  base: "/",
   plugins: [
     svgrPlugin(),
     react({

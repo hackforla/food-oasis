a7f5abe5f0a8143b7f6ff87d61cd093ff93fef18
saumildhankar <saumil.dhankar@yahoo.com>
2025-05-27 00:34:52 +0530

    wrapped the requests under admin role

--- server/app/routes/announcements-router.ts
+++ server/app/routes/announcements-router.ts
@@ -1,11 +1,28 @@
 import { Router } from "express";
 import announcementsController from "../controllers/announcements-controller";
+import jwtSession from "../../middleware/jwt-session";
+import { requestValidationMiddleware } from "../../middleware/request-validation-middlewares";
+import { AnnouncementsPostRequestSchema } from "../validation-schema/announcements-schema";
+
 
 const router = Router();
 
 router.get("/", announcementsController.getAll);
-router.post("/", announcementsController.post);
-router.delete("/:id", announcementsController.remove);
-router.put("/:id", announcementsController.update);
+router.post(
+  "/",
+  jwtSession.validateUserHasRequiredRoles(["admin"]),
+  requestValidationMiddleware(AnnouncementsPostRequestSchema),
+  announcementsController.post
+);
+router.delete(
+  "/:id",
+  jwtSession.validateUserHasRequiredRoles(["admin"]),
+  announcementsController.remove
+);
+router.put(
+  "/:id",
+  jwtSession.validateUserHasRequiredRoles(["admin"]),
+  announcementsController.update
+);
 
 export default router;

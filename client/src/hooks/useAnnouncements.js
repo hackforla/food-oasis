ff38a1a41813b022f4234bebd3ddace0b84adc26
Saumil Dhankar <saumil.dhankar@yahoo.com>
2025-08-12 10:05:00 +0530

    fixed name of announcement toggle (#2519)
    fixed toggle and shuffle
    
    moved data transformation to custom hook
    
    removed comment
    
    removed states and useeffect

--- client/src/hooks/useAnnouncements.js
+++ client/src/hooks/useAnnouncements.js
@@ -10,8 +10,17 @@ export const useAnnouncements = () => {
     const fetchApi = async () => {
       setLoading(true);
       try {
-        const announcements = await announcementService.getAllAnnouncements();
-        setData(announcements);
+        const announcements = await announcementService.getAllAnnouncements();        
+        const processed = announcements
+          .slice()
+          .sort((a, b) => a.id - b.id)
+          .map((announcement) => ({
+            announcementId: announcement.id,
+            title: announcement.title,
+            description: announcement.description,
+            is_enabled: announcement.is_enabled,
+          }));
+        setData(processed);
         setLoading(false);
       } catch (err) {
         setError(err);

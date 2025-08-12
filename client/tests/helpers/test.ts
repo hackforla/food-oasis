fe3d499029b59d56ae381e887863e17e21220c8a
hanapotski <hannahjoy.zulueta@gmail.com>
2025-08-11 19:34:29 -0700

    Try to fix failing test

--- client/tests/helpers/test.ts
+++ client/tests/helpers/test.ts
@@ -1,4 +1,5 @@
-import { expect, Page, test as playwrightTest } from "@playwright/test";
+import { expect, test as playwrightTest } from "@playwright/test";
+import type { Page } from "@playwright/test";
 import mockRequests from "./mocks";
 
 declare global {

41cb1d138a36310dc52bd2b251b6813deb08fcad
saumildhankar <saumil.dhankar@yahoo.com>
2025-08-18 05:54:41 +0530

    added schema and route
    added minlength for empty string validation
    
    server side validation for update

--- server/app/routes/parent-organization-router.ts
+++ server/app/routes/parent-organization-router.ts
@@ -1,18 +1,25 @@
 import { Router } from "express";
 const router = Router();
 import parentOrganizationController from "../controllers/parent-organization-controller";
+import { requestValidationMiddleware } from "../../middleware/request-validation-middlewares";
+import { ParentOrganizationPostRequestSchema } from "../validation-schema/parent-organization-schema";
+import { ParentOrganizationPutRequestSchema } from "../validation-schema/parent-organization-schema";
 
 import jwtSession from "../../middleware/jwt-session";
 
 router.get("/:tenantId", parentOrganizationController.getAllByTenantId);
+
 router.post(
   "/",
   jwtSession.validateUserHasRequiredRoles(["admin"]),
+  requestValidationMiddleware(ParentOrganizationPostRequestSchema),
   parentOrganizationController.insert
 );
+
 router.put(
   "/:id",
   jwtSession.validateUserHasRequiredRoles(["admin"]),
+  requestValidationMiddleware(ParentOrganizationPutRequestSchema),
   parentOrganizationController.update
 );
 

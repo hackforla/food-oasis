ae1a8f9f63430eb4ae63e189e571114e3e9a0037
June Zhao <bz.bijun@gmail.com>
2025-10-04 14:24:57 -0700

    refactor: rewrite AccountAutocomplete and AssignDialog components with TypeScript

--- client/src/components/Admin/AccountAutocomplete.tsx
+++ client/src/components/Admin/AccountAutocomplete.tsx
@@ -1,15 +1,30 @@
 import { Autocomplete, TextField } from "@mui/material";
 import { useAccounts } from "../../hooks/useAccounts";
 
+interface Account {
+  id: string | number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+interface AccountAutocompleteProps {
+  accountId: string | number | null;
+  setAccount?: (account: Account | null) => void;
+  setAccountId?: (id: string | number | null) => void;
+  label?: string;
+  name?: string
+}
+
 export default function AccountAutocomplete({
   accountId,
   setAccount,
   setAccountId,
   label,
-}) {
+  name
+}: AccountAutocompleteProps) {
   const { data: accounts } = useAccounts();
 
-  const handleChange = (event, value) => {
+  const handleChange = (_event: unknown,value: Account | null) => {
     setAccount && setAccount(value);
     setAccountId && setAccountId(value ? value.id : null);
   };
@@ -21,7 +36,7 @@ export default function AccountAutocomplete({
           value={accounts.find((acct) => acct.id === accountId) || null}
           onChange={handleChange}
           options={accounts}
-          getOptionLabel={(option) =>
+          getOptionLabel={(option: Account) =>
             `${option.lastName}, ${option.firstName} (${option.email})`
           }
           style={{ width: "100%" }}
---
+++ client/src/components/Admin/AssignDialog.tsx
@@ -0,0 +1,67 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogProps,
+  DialogTitle,
+} from "@mui/material";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import AccountAutocomplete from "./AccountAutocomplete";
+
+interface AssignDialogProps extends Omit<DialogProps, "onClose"> {
+  open: boolean;
+  onClose: (value: string | number | false) => void;
+}
+
+function AssignDialog(props: AssignDialogProps) {
+  const { onClose, open, ...other } = props;
+  const [accountId, setAccountId] = useState<string | number | null>(null);
+
+  const handleCancel = () => {
+    onClose(false);
+  };
+
+  const handleAssign = () => {
+    if (accountId !== null) {
+      onClose(accountId);
+    }
+
+    return (
+      <Dialog
+        onClose={handleCancel}
+        disableEscapeKeyDown
+        fullWidth
+        maxWidth="sm"
+        aria-labelledby="confirmation-dialog-title"
+        open={open}
+        {...other}
+      >
+        <DialogTitle id="confirmation-dialog-title">Assign to:</DialogTitle>
+        <DialogContent dividers>
+          <AccountAutocomplete
+            name="assignedLoginId"
+            accountId={accountId}
+            setAccountId={(assignedAccountId) =>
+              setAccountId(assignedAccountId)
+            }
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" autoFocus onClick={handleCancel}>
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            onClick={handleAssign}
+            disabled={!accountId}
+          >
+            Assign
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+}
+export default AssignDialog;

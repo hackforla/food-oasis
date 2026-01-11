5a356d5f70375b32751b4b17f3e05b1107b4f1dc
June Zhao <60997220+junjun107@users.noreply.github.com>
2026-01-10 16:43:40 -0800

    Feat/june/auth state update playwright (#2608)
    * update mocked login state
    
    * update table header content
    
    * test: load app after mock login setup

--- client/tests/surveySnackbarRouting.spec.ts
+++ client/tests/surveySnackbarRouting.spec.ts
@@ -2,29 +2,6 @@ import { expect } from "@playwright/test";
 import test from "./helpers/test";
 import mockRequests from "./helpers/mocks";
 
-// test("survey only shows on user facing routes and not within admin routes", async ({
-//   page,
-//   helpers,
-// }) => {
-//   await page.addInitScript(() => {
-//     localStorage.setItem(
-//       "featureFlags",
-//       JSON.stringify({ userFeedbackSurvey: true })
-//     );
-//   });
-
-//   await mockRequests(page);
-//   await page.goto("/");
-
-//   const survey = page
-//     .getByRole("alert")
-//     .filter({ hasText: /Help us improve!/i });
-//   await expect(survey).toBeVisible();
-//   await helpers.mockLogin();
-
-//   await expect(survey).toHaveCount(0);
-// });
-
 test("SurveySnackbar shows on user routes and hides on admin routes", async ({
   page,
   helpers,
@@ -33,9 +10,11 @@ test("SurveySnackbar shows on user routes and hides on admin routes", async ({
   await page.goto("/");
   await expect(page.getByText("Help us improve!")).toBeVisible();
   await helpers.mockLogin();
-  await expect(page).toHaveURL(/\/admin\//);
+
   const surveyAfterLogin = page
     .getByRole("alert")
     .filter({ hasText: /help us improve!/i });
+  await page.goto("/admin/verificationadmin");
   await expect(surveyAfterLogin).toHaveCount(0);
+  await expect(page).toHaveURL(/\/admin\//);
 });

1037795aaee9bc1386681693f18f3fff89106bb7
June Zhao <60997220+junjun107@users.noreply.github.com>
2026-01-26 15:40:00 -0800

    convert features and related files to typescript (#2623)

--- client/src/services/feature-service.tsx
+++ client/src/services/feature-service.tsx
@@ -1,30 +1,35 @@
 import axios from "axios";
 
+export interface Feature {
+  id: number;
+  name: string;
+  is_enabled: boolean;
+}
+
 const baseUrl = "/api/features";
 
-export const getAllFeatures = async () => {
+export const getAllFeatures = async (): Promise<Feature[]> => {
   try {
     const response = await axios.get(`${baseUrl}`);
     return response.data;
-  } catch (error) {
+  } catch (error: any) {
     throw new Error(error.message);
   }
 };
 
-export const post = async (feature) => {
+export const post = async (feature: Partial<Feature>) => {
   const response = await axios.post(`${baseUrl}`, {
     ...feature,
   });
   return response.data;
 };
 
-export const remove = async (id) => {
+export const remove = async (id: number) => {
   const response = await axios.delete(`${baseUrl}/${id}`);
   return response.data;
 };
 
-export const update = async (id, feature) => {
+export const update = async (id: number, feature: Partial<Feature>) => {
   const response = await axios.put(`${baseUrl}/${id}`, feature);
   return response.data;
 };
- 

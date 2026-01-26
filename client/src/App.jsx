0663d0f8c629283dab8cd13a59c973e94e63986f
Abhiraj <abhirajmarne11@gmail.com>
2026-01-26 15:45:53 +0530

    Fix: Scroll position not resetting when switching sidebar tabs #2662

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,15 @@ import { SiteProvider } from "./contexts/siteContext";
 import { ToasterProvider } from "contexts/toasterContext";
 import { UserProvider } from "contexts/userContext";
 import { HelmetProvider } from "react-helmet-async";
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "theme";
 import * as analytics from "./services/analytics";
 import { AppStateProvider } from "./appReducer";
 import SEO from "./components/SEO";
 import AppRoutes from "./Routes";
 import { MapProvider } from "react-map-gl";
+
+import ScrollToTop from "./components/Layout/ScrollToTop";
 
 function App() {
   useEffect(() => {
@@ -34,6 +34,7 @@ function App() {
                 />
                 <MapProvider>
                   <Router>
+                    <ScrollToTop />
                     <AppRoutes />
                   </Router>
                 </MapProvider>

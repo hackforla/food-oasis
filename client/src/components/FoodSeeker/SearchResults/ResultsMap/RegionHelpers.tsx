f97de3b17151f6a65c970a0b76290c9988f38fe2
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2026-03-07 00:46:52 -0500

    convert resultsMap folder to typescript (#2687)

--- client/src/components/FoodSeeker/SearchResults/ResultsMap/RegionHelpers.tsx
+++ client/src/components/FoodSeeker/SearchResults/ResultsMap/RegionHelpers.tsx
@@ -2,7 +2,7 @@ export const REGIONS_LAYER_ID = "markers";
 
 export const regionFillStyle = {
   id: "point",
-  type: "fill",
+  type: "fill" as const,
   paint: {
     "fill-color": "#d0d0d0",
     "fill-opacity": 0.4,
@@ -11,7 +11,7 @@ export const regionFillStyle = {
 
 export const regionBorderStyle = {
   id: "regionBorder",
-  type: "line",
+  type: "line" as const,
   paint: {
     "line-color": "#808080",
     "line-width": 2,

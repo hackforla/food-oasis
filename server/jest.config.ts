ee7a0e54fa2091e9ac4418f0d39049b7fea5d2b5
Luigi Olianas <160989051+Giolii@users.noreply.github.com>
2026-03-31 15:18:00 -0700

    Fix server Jest config for ESM dependencies

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,2 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require("dotenv").config();
+
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(ts|tsx|js)$": "babel-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "json"],
+  testPathIgnorePatterns: ["/node_modules/", "/build/"],
+  transformIgnorePatterns: [
+    "/node_modules/(?!(camelcase-keys|camelcase|map-obj|quick-lru|uuid)/)",
+  ],
+};

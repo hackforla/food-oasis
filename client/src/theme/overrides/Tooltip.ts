757aed9086e3541e0c6c680074b3ccac9b9edc85
June Zhao <bz.bijun@gmail.com>
2026-04-20 13:44:55 -0700

    Convert theme files to TypeScript & implement component overrides

--- client/src/theme/overrides/Tooltip.ts
+++ client/src/theme/overrides/Tooltip.ts
@@ -1,4 +1,6 @@
-export default function Tooltip(theme) {
+import type { Theme } from "@mui/material/styles";
+
+export default function Tooltip(theme: Theme) {
   return {
     MuiTooltip: {
       defaultProps: {

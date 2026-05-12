cc39327f926f680ce98f1ab4366dcf2d798818e9
Luigi Olianas <160989051+Giolii@users.noreply.github.com>
2026-05-12 11:19:22 -0700

    Fix address bar bug (#2773)

--- client/src/hooks/useMapboxGeocoder.js
+++ client/src/hooks/useMapboxGeocoder.js
@@ -5,7 +5,7 @@ import {
   MAPBOX_ACCESS_TOKEN,
   DEFAULT_VIEWPORTS,
 } from "helpers/Constants";
-import { useCallback, useReducer } from "react";
+import { useCallback, useReducer, useRef } from "react";
 
 const baseUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places`;
 
@@ -13,6 +13,7 @@ const initialState = {
   isLoading: false,
   error: false,
   mapboxResults: [],
+  resultsQuery: "",
 };
 
 const actionTypes = {
@@ -24,13 +25,14 @@ const actionTypes = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case actionTypes.FETCH_REQUEST:
-      return { ...state, isLoading: true };
+      return { ...state, isLoading: true, resultsQuery: action.query };
     case actionTypes.FETCH_SUCCESS:
       return {
         ...state,
         error: false,
         isLoading: false,
         mapboxResults: action.results,
+        resultsQuery: action.query,
       };
     case actionTypes.FETCH_FAILURE:
       console.error(action.error);
@@ -41,35 +43,112 @@ function reducer(state = initialState, action) {
 }
 
 export function useMapboxGeocoder() {
-  const [{ isLoading, error, mapboxResults }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ isLoading, error, mapboxResults, resultsQuery }, dispatch] =
+    useReducer(reducer, initialState);
+  const latestQueryRef = useRef("");
+
+  const performFetch = useCallback(async (searchString) => {
+    const bbox = DEFAULT_VIEWPORTS[TENANT_ID].bbox;
+    const mapboxUrl = `${baseUrl}/${searchString}.json?bbox=${bbox}&access_token=${MAPBOX_ACCESS_TOKEN}`;
+
+    try {
+      const response = await axios.get(mapboxUrl);
+      const results = response.data.features;
+
+      if (latestQueryRef.current === searchString) {
+        dispatch({
+          type: actionTypes.FETCH_SUCCESS,
+          results,
+          query: searchString,
+        });
+      }
+
+      return results;
+    } catch (error) {
+      if (latestQueryRef.current === searchString) {
+        dispatch({ type: actionTypes.FETCH_FAILURE, error });
+      }
+      return [];
+    }
+  }, []);
 
   const debouncedFetch = useCallback(
     debounce(
       async (searchString) => {
-        const bbox = DEFAULT_VIEWPORTS[TENANT_ID].bbox;
-        const mapboxUrl = `${baseUrl}/${searchString}.json?bbox=${bbox}&access_token=${MAPBOX_ACCESS_TOKEN}`;
-
-        try {
-          const response = await axios.get(mapboxUrl);
-          dispatch({
-            type: actionTypes.FETCH_SUCCESS,
-            results: response.data.features,
-          });
-        } catch (error) {
-          dispatch({ type: actionTypes.FETCH_FAILURE, error });
-        }
+        await performFetch(searchString);
       },
       { wait: 300 }
     ),
-    []
+    [performFetch]
   );
-  const fetchMapboxResults = useCallback((searchString) => {
-    dispatch({ type: actionTypes.FETCH_REQUEST });
-    debouncedFetch(searchString);
-  }, []);
 
-  return { error, isLoading, mapboxResults, fetchMapboxResults };
+  const clearResults = useCallback(() => {
+    debouncedFetch.cancel?.();
+    latestQueryRef.current = "";
+    dispatch({
+      type: actionTypes.FETCH_SUCCESS,
+      results: [],
+      query: "",
+    });
+  }, [debouncedFetch]);
+
+  const searchMapboxResults = useCallback(
+    (searchString) => {
+      const normalizedSearchString = searchString.trim();
+      latestQueryRef.current = normalizedSearchString;
+
+      if (!normalizedSearchString) {
+        clearResults();
+        return;
+      }
+
+      dispatch({
+        type: actionTypes.FETCH_REQUEST,
+        query: normalizedSearchString,
+      });
+      debouncedFetch(normalizedSearchString);
+    },
+    [clearResults, debouncedFetch]
+  );
+
+  const ensureMapboxResults = useCallback(
+    async (searchString) => {
+      const normalizedSearchString = searchString.trim();
+
+      if (!normalizedSearchString) {
+        clearResults();
+        return [];
+      }
+
+      if (!isLoading && resultsQuery === normalizedSearchString) {
+        return mapboxResults;
+      }
+
+      latestQueryRef.current = normalizedSearchString;
+      debouncedFetch.cancel?.();
+      dispatch({
+        type: actionTypes.FETCH_REQUEST,
+        query: normalizedSearchString,
+      });
+
+      return performFetch(normalizedSearchString);
+    },
+    [
+      clearResults,
+      debouncedFetch,
+      isLoading,
+      mapboxResults,
+      performFetch,
+      resultsQuery,
+    ]
+  );
+
+  return {
+    error,
+    isLoading,
+    mapboxResults,
+    resultsQuery,
+    searchMapboxResults,
+    ensureMapboxResults,
+  };
 }
